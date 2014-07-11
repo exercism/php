afece2baa77f1230db1976025217093ce95f0214
Jordon Smith <jrd@mockra.net>
2014-07-11 20:46:08 +0100

    Add hamming problem

---
+++ Hamming/Example.php
@@ -0,0 +1,21 @@
+<?php
+namespace Exercism\Hamming;
+
+class HammingComparator
+{
+    /**
+     * @param string $a
+     * @param string $b
+     * @return int distance
+     */
+    public static function distance($a, $b)
+    {
+        $shortest = min(array(mb_strlen($a), mb_strlen($b)));
+        return count(
+            array_diff_assoc(
+                str_split(mb_substr($a, 0, $shortest)),
+                str_split(mb_substr($b, 0, $shortest))
+            )
+        );
+    }
+}
