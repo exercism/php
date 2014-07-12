a9a453c36828046d3c821a030f51462bd9af769c
Jordon Smith <jrd@mockra.net>
2014-07-12 08:57:15 +0100

    Add leap problem

---
+++ Leap/Example.php
@@ -0,0 +1,28 @@
+<?php
+namespace Exercism\Leap;
+
+class Year
+{
+    private $year;
+
+    /**
+     * @param int $year
+     */
+    public function __construct($year)
+    {
+        $this->year = $year;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLeap()
+    {
+        return $this->isDivisibleBy(4) && (!$this->isDivisibleBy(100) || $this->isDivisibleBy(400));
+    }
+
+    private function isDivisibleBy($n)
+    {
+        return $this->year % $n === 0;
+    }
+}
