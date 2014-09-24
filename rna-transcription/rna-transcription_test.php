e5c63ea5f7a0a9836051f703a7778491d23b3ac1
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-09-24 11:26:42 +0100

    Add RNA Transcription Exercise

---
+++ rna-transcription/rna-transcription_test.php
@@ -0,0 +1,74 @@
+<?php
+
+require "complement.php";
+
+class ComplementTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRNAComplementOfCytosineIsGuanine()
+    {
+        $this->assertSame(Complement::ofDna('C'), 'G');
+    }
+
+    public function testRNAComplementOfGuanineIsCytosine()
+    {
+        $this->markTestSkipped();
+
+        $this->assertSame(Complement::ofDna('G'), 'C');
+    }
+
+    public function testRNAComplementOfThymineIsAdenine()
+    {
+        $this->markTestSkipped();
+
+        $this->assertSame(Complement::ofDna('T'), 'A');
+    }
+
+    public function testRNAComplementOfAdenineIsUracil()
+    {
+        $this->markTestSkipped();
+
+        $this->assertSame(Complement::ofDna('A'), 'U');
+    }
+
+    public function testRNAComplement()
+    {
+        $this->markTestSkipped();
+
+        $this->assertSame(Complement::ofDna('ACGTGGTCTTAA'), 'UGCACCAGAAUU');
+    }
+
+    public function testDNAComplementOfCytosineIsGuanine()
+    {
+        $this->markTestSkipped();
+
+        $this->assertSame(Complement::ofRna('C'), 'G');
+    }
+
+    public function testDNAComplementOfGuanineIsCytosine()
+    {
+        $this->markTestSkipped();
+
+        $this->assertSame(Complement::ofRna('G'), 'C');
+    }
+
+    public function testDNAComplementOfUracilIsAdenine()
+    {
+        $this->markTestSkipped();
+
+        $this->assertSame(Complement::ofRna('U'), 'A');
+    }
+
+    public function testDNAComplementOfAdenineIsThymine()
+    {
+        $this->markTestSkipped();
+
+        $this->assertSame(Complement::ofRna('A'), 'T');
+    }
+
+    public function testDNAComplement()
+    {
+        $this->markTestSkipped();
+
+        $this->assertSame(Complement::ofRna('UGAACCCGACAUG'), 'ACTTGGGCTGTAC');
+    }
+}
