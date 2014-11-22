b93a272f565530bd6e46b7b806ab8ded7b26e4b9
Dog <dog@kibblesandbytes.com>
2014-11-21 22:43:52 -0800

    Leap: Updated exercise to be a function rather than a class

--- leap/example.php
+++ leap/example.php
@@ -1,27 +1,9 @@
 <?php
 
-class Year
+/**
+ * @return bool
+ */
+function isLeap($year)
 {
-    private $year;
-
-    /**
-     * @param int $year
-     */
-    public function __construct($year)
-    {
-        $this->year = $year;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isLeap()
-    {
-        return $this->isDivisibleBy(4) && (!$this->isDivisibleBy(100) || $this->isDivisibleBy(400));
-    }
-
-    private function isDivisibleBy($n)
-    {
-        return $this->year % $n === 0;
-    }
+    return !($year % 4) && (!!($year % 100) || !($year % 400));
 }
