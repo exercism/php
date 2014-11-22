1f26e08818f9e0bbc077c64b1de2f272c9f6c913
Dog <dog@kibblesandbytes.com>
2014-11-21 22:43:52 -0800

    Leap: Updated exercise to be a function rather than a class

--- leap/leap_test.php
+++ leap/leap_test.php
@@ -6,26 +6,26 @@ class YearTest extends \PHPUnit_Framework_TestCase
 {
     public function testLeapYear()
     {
-        $this->assertTrue((new Year(1996))->isLeap());
+        $this->assertTrue(isLeap(1996));
     }
 
     public function testNonLeapYear()
     {
-        $this->assertFalse((new Year(1997))->isLeap());
+        $this->assertFalse(isLeap(1997));
     }
 
     public function testNonLeapEvenYear()
     {
-        $this->assertFalse((new Year(1998))->isLeap());
+        $this->assertFalse(isLeap(1998));
     }
 
     public function testCentury()
     {
-        $this->assertFalse((new Year(1900))->isLeap());
+        $this->assertFalse(isLeap(1900));
     }
 
     public function testFourthCentury()
     {
-        $this->assertTrue((new Year(2400))->isLeap());
+        $this->assertTrue(isLeap(2400));
     }
 }
