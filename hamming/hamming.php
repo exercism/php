8b4221339fad678b5c8ec620e7d8ef2775081979
Pete McFarlane <pmcfarlane@workanyware.co.uk>
2015-03-19 15:45:37 +0000

    Added skeleton file for Hamming

---
+++ hamming/hamming.php
@@ -0,0 +1,13 @@
+<?php
+
+//
+// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+function distance($a, $b)
+{
+    //
+    // YOUR CODE GOES HERE
+    //
+}
