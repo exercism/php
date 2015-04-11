b68a1380a33b34e7dd0465021e74e0f93691e5a0
Peter Aba <peteraba@users.noreply.github.com>
2015-04-11 13:52:30 +0200

    Fixing mixed up expected and actual values for Gigasecond test

--- gigasecond/gigasecond_test.php
+++ gigasecond/gigasecond_test.php
@@ -17,7 +17,7 @@ class GigasecondTest extends \PHPUnit_Framework_TestCase
         $date = GigasecondTest::dateSetup("2011-04-25");
         $gs = from($date);
 
-        $this->assertSame($gs->format("Y-m-d H:i:s"), "2043-01-01 01:46:40");
+        $this->assertSame("2043-01-01 01:46:40", $gs->format("Y-m-d H:i:s"));
     }
 
     public function test2()
@@ -26,7 +26,7 @@ class GigasecondTest extends \PHPUnit_Framework_TestCase
         $date = GigasecondTest::dateSetup("1977-06-13");
         $gs = from($date);
 
-        $this->assertSame($gs->format("Y-m-d H:i:s"), "2009-02-19 01:46:40");
+        $this->assertSame("2009-02-19 01:46:40", $gs->format("Y-m-d H:i:s"));
     }
 
     public function test3()
@@ -35,7 +35,7 @@ class GigasecondTest extends \PHPUnit_Framework_TestCase
         $date = GigasecondTest::dateSetup("1959-7-19");
         $gs = from($date);
 
-        $this->assertSame($gs->format("Y-m-d H:i:s"), "1991-03-27 01:46:40");
+        $this->assertSame("1991-03-27 01:46:40", $gs->format("Y-m-d H:i:s"));
     }
 
     public function test4()
@@ -44,7 +44,7 @@ class GigasecondTest extends \PHPUnit_Framework_TestCase
         $date = GigasecondTest::dateSetup("2015-01-24 22:00:00");
         $gs = from($date);
 
-        $this->assertSame($gs->format("Y-m-d H:i:s"), "2046-10-02 23:46:40");
+        $this->assertSame("2046-10-02 23:46:40", $gs->format("Y-m-d H:i:s"));
     }
 
     public function test5()
@@ -53,7 +53,7 @@ class GigasecondTest extends \PHPUnit_Framework_TestCase
         $date = GigasecondTest::dateSetup("2015-01-24 23:59:59");
         $gs = from($date);
 
-        $this->assertSame($gs->format("Y-m-d H:i:s"), "2046-10-03 01:46:39");
+        $this->assertSame("2046-10-03 01:46:39", $gs->format("Y-m-d H:i:s"));
     }
 
     public function testYourself()
@@ -62,6 +62,6 @@ class GigasecondTest extends \PHPUnit_Framework_TestCase
         $your_birthday = GigasecondTest::dateSetup("your_birthday");
         $gs = from($your_birthday);
 
-        $this->assertSame($gs->format("Y-m-d H:i:s"), "2046-10-03 01:46:39");
+        $this->assertSame("2046-10-03 01:46:39", $gs->format("Y-m-d H:i:s"));
     }
 }
