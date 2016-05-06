d9996887a79a7e74a726b06852f92e65971cf41a
ecrmnn <danieleckermann@gmail.com>
2016-05-06 19:27:02 +0200

    WordCount: Tests and Example

---
+++ exercises/word-count/example.php
@@ -0,0 +1,18 @@
+<?php
+
+function wordCount($phrase)
+{
+    $count = [];
+
+    $phrase = preg_replace(['/[^a-zA-Z0-9]+/', '/\s\s/'], ' ', mb_strtolower($phrase));
+
+    foreach (explode(' ', $phrase) as $word) {
+        if (!array_key_exists($word, $count) && !empty($word)) {
+            $count[$word] = 1;
+        } elseif (array_key_exists($word, $count)) {
+            $count[$word]++;
+        }
+    }
+
+    return $count;
+}
---
+++ exercises/word-count/word-count_test.php
@@ -0,0 +1,71 @@
+<?php
+
+require "word-count.php";
+
+class WordCountTest extends PHPUnit_Framework_TestCase
+{
+    public function testCountOneWord()
+    {
+        $this->assertEquals(['word' => 1], wordCount('word'));
+    }
+
+    public function testCountOneOfEachWord()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(['one' => 1, 'of' => 1, 'each' => 1], wordCount('one of each'));
+    }
+
+    public function testMultipleOccurrencesOfAWord()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(
+            ['one' => 1, 'fish' => 4, 'two' => 1, 'red' => 1, 'blue' => 1],
+            wordCount('one fish two fish red fish blue fish')
+        );
+    }
+
+    public function testIgnorePunctuation()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(
+            ['car' => 1, 'carpet' => 1, 'as' => 1, 'java' => 1, 'javascript' => 1],
+            wordCount('car : carpet as java : javascript!!&@$%^&')
+        );
+    }
+
+    public function testIncludeNumbers()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(['1' => 1, '2' => 1, 'testing' => 2], wordCount('testing, 1, 2 testing'));
+    }
+
+    public function testNormalizeCase()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(['go' => 3, 'stop' => 2], wordCount('go Go GO Stop stop'));
+    }
+
+    public function testCountsMultiline()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(['hello' => 1, 'world' => 1], wordCount("hello\nworld"));
+    }
+
+    public function testCountsTabs()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(['hello' => 1, 'world' => 1], wordCount("hello\tworld"));
+    }
+
+    public function testCountsMultipleSpacesAsOne()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(['hello' => 1, 'world' => 1], wordCount('hello  world'));
+    }
+
+    public function testDoesNotCountLeadingOrTrailingWhitespace()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(['introductory' => 1, 'course' => 1], wordCount("\t\tIntroductory Course      "));
+    }
+}
