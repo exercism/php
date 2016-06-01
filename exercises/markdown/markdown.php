d341a370d5afb1942e7b7cf6258e9ae75339f1f8
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2016-06-01 18:20:33 +0100

    Add markdown parser exercise

---
+++ exercises/markdown/markdown.php
@@ -0,0 +1,82 @@
+<?php
+
+function parseMarkdown($markdown)
+{
+    $lines = explode("\n", $markdown);
+
+    $isInList = false;
+
+    foreach ($lines as &$line) {
+        if (preg_match('/^######(.*)/', $line, $matches)) {
+            $line = "<h6>" . trim($matches[1]) . "</h6>";
+        } elseif (preg_match('/^##(.*)/', $line, $matches)) {
+            $line = "<h2>" . trim($matches[1]) . "</h2>";
+        } elseif (preg_match('/^#(.*)/', $line, $matches)) {
+            $line = "<h1>" . trim($matches[1]) . "</h1>";
+        }
+
+        if (preg_match('/\*(.*)/', $line, $matches)) {
+            if (!$isInList) {
+                $isInList = true;
+                $isBold = false;
+                $isItalic = false;
+                if (preg_match('/(.*)__(.*)__(.*)/', $matches[1], $matches2)) {
+                    $matches[1] = $matches2[1] . '<em>' . $matches2[2] . '</em>' . $matches2[3];
+                    $isBold = true;
+                }
+
+                if (preg_match('/(.*)_(.*)_(.*)/', $matches[1], $matches3)) {
+                    $matches[1] = $matches3[1] . '<i>' . $matches3[2] . '</i>' . $matches3[3];
+                    $isItalic = true;
+                }
+
+                if ($isItalic || $isBold) {
+                    $line = "<ul><li>" . trim($matches[1]) . "</li>";
+                } else {
+                    $line = "<ul><li><p>" . trim($matches[1]) . "</p></li>";
+                }
+
+            } else {
+                $isBold = false;
+                $isItalic = false;
+                if (preg_match('/(.*)__(.*)__(.*)/', $matches[1], $matches2)) {
+                    $matches[1] = $matches2[1] . '<em>' . $matches2[2] . '</em>' . $matches2[3];
+                    $isBold = true;
+                }
+
+                if (preg_match('/(.*)_(.*)_(.*)/', $matches[1], $matches3)) {
+                    $matches[1] = $matches3[1] . '<i>' . $matches3[2] . '</i>' . $matches3[3];
+                    $isItalic = true;
+                }
+
+                if ($isItalic || $isBold) {
+                    $line = "<li>" . trim($matches[1]) . "</li>";
+                } else {
+                    $line = "<li><p>" . trim($matches[1]) . "</p></li>";
+                }
+            }
+        } else {
+            if ($isInList) {
+                $line = "</ul>" . $line;
+                $isInList = false;
+            }
+        }
+
+        if (!preg_match('/<h|<ul|<p|<li/', $line)) {
+            $line = "<p>$line</p>";
+        }
+
+        if (preg_match('/(.*)__(.*)__(.*)/', $line, $matches)) {
+            $line = $matches[1] . '<em>' . $matches[2] . '</em>' . $matches[3];
+        }
+
+        if (preg_match('/(.*)_(.*)_(.*)/', $line, $matches)) {
+            $line = $matches[1] . '<i>' . $matches[2] . '</i>' . $matches[3];
+        }
+    }
+    $html = join($lines);
+    if ($isInList) {
+        $html .= '</ul>';
+    }
+    return $html;
+}
---
+++ exercises/markdown/markdown_test.php
@@ -0,0 +1,57 @@
+<?php
+
+require 'markdown.php';
+
+class MarkdownTest extends \PHPUnit_Framework_TestCase
+{
+    public function testParsingParagraph()
+    {
+        $this->assertEquals('<p>This will be a paragraph</p>', parseMarkdown('This will be a paragraph'));
+    }
+
+    public function testParsingItalics()
+    {
+        $this->assertEquals('<p><i>This will be italic</i></p>', parseMarkdown('_This will be italic_'));
+    }
+
+    public function testParsingBoldText()
+    {
+        $this->assertEquals('<p><em>This will be bold</em></p>', parseMarkdown('__This will be bold__'));
+    }
+
+    public function testMixedNormalItalicsAndBoldText()
+    {
+        $this->assertEquals('<p>This will <i>be</i> <em>mixed</em></p>', parseMarkdown('This will _be_ __mixed__'));
+    }
+
+    public function testWithH1Headerlevel()
+    {
+        $this->assertEquals('<h1>This will be an h1</h1>', parseMarkdown('# This will be an h1'));
+    }
+
+    public function testWithH2Headerlevel()
+    {
+        $this->assertEquals('<h2>This will be an h2</h2>', parseMarkdown('## This will be an h2'));
+    }
+
+    public function testWithH6Headerlevel()
+    {
+        $this->assertEquals('<h6>This will be an h6</h6>', parseMarkdown('###### This will be an h6'));
+    }
+
+    public function testUnorderedLists()
+    {
+        $this->assertEquals(
+            '<ul><li><p>Item 1</p></li><li><p>Item 2</p></li></ul>',
+            parseMarkdown("* Item 1\n* Item 2")
+        );
+    }
+
+    public function testWithALittleBitOfEverything()
+    {
+        $this->assertEquals(
+            '<h1>Header!</h1><ul><li><em>Bold Item</em></li><li><i>Italic Item</i></li></ul>',
+            parseMarkdown("# Header!\n* __Bold Item__\n* _Italic Item_")
+        );
+    }
+}
