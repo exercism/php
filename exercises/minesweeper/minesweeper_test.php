a171300d8cd25f681b2225e889ddc1425f35c6ff
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2016-06-22 12:17:55 +0100

    Add minesweeper (#92)

---
+++ exercises/minesweeper/minesweeper_test.php
@@ -0,0 +1,199 @@
+<?php
+
+require 'minesweeper.php';
+
+class MinesweeperTest extends PHPUnit_Framework_TestCase
+{
+    public function testAnEmptyBoard()
+    {
+        $emptyBoard = '
++--+
+|  |
++--+
+';
+        $this->assertSame($emptyBoard, solve($emptyBoard));
+    }
+
+    public function testAnIncompleteSideBorderThrowsAnException()
+    {
+        $this->markTestSkipped();
+        $incompleteBoard = '
++--+
+   |
++--+
+';
+        $this->setExpectedException('InvalidArgumentException');
+
+        solve($incompleteBoard);
+    }
+
+    public function testAnIncompleteTopBorderThrowsAnException()
+    {
+        $this->markTestSkipped();
+        $incompleteBoard = '
++ -+
+|  |
++--+
+';
+        $this->setExpectedException('InvalidArgumentException');
+
+        solve($incompleteBoard);
+    }
+
+    public function testAMissingCornerThrowsAnException()
+    {
+        $this->markTestSkipped();
+        $incompleteBoard = '
++--
+|  |
++--+
+';
+        $this->setExpectedException('InvalidArgumentException');
+
+        solve($incompleteBoard);
+    }
+
+    public function testABoardWithLessThan2SquaresThrowsAnException()
+    {
+        $this->markTestSkipped();
+        $tinyBoard = '
++-+
+| |
++-+
+';
+        $this->setExpectedException('InvalidArgumentException');
+
+        solve($tinyBoard);
+    }
+
+    public function testRowsOfSameLength($value = '')
+    {
+        $this->markTestSkipped();
+        $unequalBoard = '
++---+
+|   |
+| |
++---+
+';
+
+        $this->setExpectedException('InvalidArgumentException');
+
+        solve($unequalBoard);
+    }
+
+    public function testCanOnlyContainMines($value = '')
+    {
+        $this->markTestSkipped();
+        $badBoard = '
++---+
+|  *|
+| * |
+| ? |
++---+
+';
+
+        $this->setExpectedException('InvalidArgumentException');
+
+        solve($badBoard);
+    }
+
+    public function testBoardWithOneMineToLeft()
+    {
+        $this->markTestSkipped();
+        $oneMine = '
++--+
+|* |
++--+
+';
+
+        $expected = '
++--+
+|*1|
++--+
+';
+
+        $this->assertEquals($expected, solve($oneMine));
+    }
+
+    public function testBoardWithOneMineToRight()
+    {
+        $this->markTestSkipped();
+        $oneMine = '
++--+
+| *|
++--+
+';
+
+        $expected = '
++--+
+|1*|
++--+
+';
+
+        $this->assertEquals($expected, solve($oneMine));
+    }
+
+    public function testBoardWithAMineToTopAndRight()
+    {
+        $this->markTestSkipped();
+        $twoMines = '
++--+
+|* |
+| *|
++--+
+';
+
+        $expected = '
++--+
+|*2|
+|2*|
++--+
+';
+
+        $this->assertEquals($expected, solve($twoMines));
+    }
+
+    public function testBoardWithAMineToBottomAndLeftAndDiagonal()
+    {
+        $this->markTestSkipped();
+        $threeMines = '
++--+
+|* |
+|**|
++--+
+';
+
+        $expected = '
++--+
+|*3|
+|**|
++--+
+';
+
+        $this->assertEquals($expected, solve($threeMines));
+    }
+
+    public function testAComplicatedBoard()
+    {
+        $this->markTestSkipped();
+        $fourMines = '
++-----+
+| * * |
+|  *  |
+|  *  |
+|     |
++-----+
+';
+
+        $expected = '
++-----+
+|1*3*1|
+|13*31|
+| 2*2 |
+| 111 |
++-----+
+';
+
+        $this->assertEquals($expected, solve($fourMines));
+    }
+}
