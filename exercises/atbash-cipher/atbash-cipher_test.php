74cff078c248c3c8b2ce67d5b05cbaf6ce600d29
Nathaniel Hoag <info@nathanielhoag.com>
2016-09-22 19:44:26 -0400

    Convert from class to function

--- exercises/atbash-cipher/atbash-cipher_test.php
+++ exercises/atbash-cipher/atbash-cipher_test.php
@@ -6,58 +6,50 @@ class AtbashTest extends \PHPUnit_Framework_TestCase
 {
     public function testEncodeNo()
     {
-        $cipher = new Atbash();
-        $this->assertEquals('ml', $cipher->encode('no'));
+        $this->assertEquals('ml', encode('no'));
     }
 
     public function testEncodeYes()
     {
         $this->markTestSkipped();
-        $cipher = new Atbash();
-        $this->assertEquals('bvh', $cipher->encode('yes'));
+        $this->assertEquals('bvh', encode('yes'));
     }
 
     public function testEncodeOmg()
     {
         $this->markTestSkipped();
-        $cipher = new Atbash();
-        $this->assertEquals('lnt', $cipher->encode('OMG'));
+        $this->assertEquals('lnt', encode('OMG'));
     }
 
     public function testEncodeOmgWithSpaces()
     {
         $this->markTestSkipped();
-        $cipher = new Atbash();
-        $this->assertEquals('lnt', $cipher->encode('O M G'));
+        $this->assertEquals('lnt', encode('O M G'));
     }
 
     public function testEncodeLongWord()
     {
         $this->markTestSkipped();
-        $cipher = new Atbash();
-        $this->assertEquals('nrmwy oldrm tob', $cipher->encode('mindblowingly'));
+        $this->assertEquals('nrmwy oldrm tob', encode('mindblowingly'));
     }
 
     public function testEncodeNumbers()
     {
         $this->markTestSkipped();
-        $cipher = new Atbash();
-        $this->assertEquals('gvhgr mt123 gvhgr mt', $cipher->encode('Testing, 1 2 3, testing.'));
+        $this->assertEquals('gvhgr mt123 gvhgr mt', encode('Testing, 1 2 3, testing.'));
     }
 
     public function testEncodeSentence()
     {
         $this->markTestSkipped();
-        $cipher = new Atbash();
-        $this->assertEquals('gifgs rhurx grlm', $cipher->encode('Truth is fiction.'));
+        $this->assertEquals('gifgs rhurx grlm', encode('Truth is fiction.'));
     }
 
     public function testEncodeAllTheThings()
     {
         $this->markTestSkipped();
-        $cipher = new Atbash();
         $plaintext = 'The quick brown fox jumps over the lazy dog.';
         $encoded = 'gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt';
-        $this->assertEquals($encoded, $cipher->encode($plaintext));
+        $this->assertEquals($encoded, encode($plaintext));
     }
 }
--- exercises/atbash-cipher/example.php
+++ exercises/atbash-cipher/example.php
@@ -1,29 +1,26 @@
 <?php
 
-class Atbash
+function encode($string)
 {
-    public function encode($string)
-    {
-        $a_z = range('a', 'z');
-        $z_a = range('z', 'a');
-        $string = preg_replace("/[^a-z0-9]+/", "", strtolower($string));
-        $len = strlen($string);
+    $a_z = range('a', 'z');
+    $z_a = range('z', 'a');
+    $string = preg_replace("/[^a-z0-9]+/", "", strtolower($string));
+    $len = strlen($string);
 
-        $count = 0;
-        $encoded = [];
-        foreach (str_split($string) as $char) {
-            $count++;
-            if (is_numeric($char)) {
-                $encoded[] = $char;
-            }
-            if (ctype_lower($char)) {
-                $encoded[] = $z_a[array_search($char, $a_z)];
-            }
-            if ($count % 5 == 0 && $count < $len) {
-                $encoded[] = ' ';
-            }
+    $count = 0;
+    $encoded = [];
+    foreach (str_split($string) as $char) {
+        $count++;
+        if (is_numeric($char)) {
+            $encoded[] = $char;
         }
-
-        return implode('', $encoded);
+        if (ctype_lower($char)) {
+            $encoded[] = $z_a[array_search($char, $a_z)];
+        }
+        if ($count % 5 == 0 && $count < $len) {
+            $encoded[] = ' ';
+        }
     }
+
+    return implode('', $encoded);
 }
