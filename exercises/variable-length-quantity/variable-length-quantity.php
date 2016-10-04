2adfd1bb268f6230e2fdec1c35570cebea213815
Peter Matseykanets <pmatseykanets@gmail.com>
2016-10-04 01:59:17 -0400

    Add variable-length-quantity exercise.

---
+++ exercises/variable-length-quantity/variable-length-quantity.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @param int $int
+ * @return array
+ */
+function vlq_encode($int)
+{
+    // YOUR CODE GOES HERE
+}
+
+/**
+ * @param array $bytes
+ */
+function vlq_decode(array $bytes)
+{
+    // YOUR CODE GOES HERE
+}
