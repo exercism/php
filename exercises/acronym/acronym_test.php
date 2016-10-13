91e68aa7a3f0f74d838f2bda573a620363964a2b
Peter Matseykanets <pmatseykanets@users.noreply.github.com>
2016-10-13 03:24:56 -0400

    Add acronym exercise. (#118)

---
+++ exercises/acronym/acronym_test.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once 'acronym.php';
+
+class AcronymTest extends PHPUnit_Framework_TestCase
+{
+    public function testBasicTitleCase()
+    {
+        $this->assertEquals('PNG', acronym('Portable Network Graphics'));
+    }
+
+    public function testLowerCaseWord()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals('ROR', acronym('Ruby on Rails'));
+    }
+
+    public function testCamelCase()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals('HTML', acronym('HyperText Markup Language'));
+    }
+
+    public function testAllCapsWords()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals('PHP', acronym('PHP: Hypertext Preprocessor'));
+    }
+
+    public function testHyphenated()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals('CMOS', acronym('Complementary metal-oxide semiconductor'));
+    }
+
+    // Additional points for making the following tests pass
+
+    public function testOneWordIsNotAbbreviated()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEmpty(acronym('Word'));
+    }
+
+    public function testUnicode()
+    {
+        $this->markTestSkipped();
+
+        $phrase = 'Специализированная процессорная часть';
+        $this->assertEquals('СПЧ', acronym($phrase));
+    }
+}
