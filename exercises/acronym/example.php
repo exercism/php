c14e4863ef8072bea1faac72a00f5eec61f787da
Peter Matseykanets <pmatseykanets@gmail.com>
2016-10-13 02:14:15 -0400

    Add acronym exercise.

---
+++ exercises/acronym/example.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Abbreviates a phrase.
+ *
+ * @param string $phrase
+ * @return string
+ */
+function acronym($phrase)
+{
+    $matches = preg_match_all('/\p{Lu}+\p{Ll}*|\p{Ll}+/u', $phrase, $words);
+
+    if ($matches === false || $matches < 2) {
+        return '';
+    }
+
+    return array_reduce($words[0], function ($acronym, $word) {
+        return $acronym.mb_strtoupper(mb_substr($word, 0, 1));
+    });
+}
