9f57f104881cbaa690ea6dbe3964213cf570ea7e
woylie <woylie@users.noreply.github.com>
2016-10-17 23:52:25 +0200

    instantiate class only once in setup

--- exercises/pig-latin/pig-latin_test.php
+++ exercises/pig-latin/pig-latin_test.php
@@ -3,144 +3,150 @@ require_once "pig-latin.php";
 
 class PigLatinTest extends PHPUnit_Framework_TestCase
 {
+    protected $translator = null;
+
+    public function setUp()
+    {
+        $this->translator = new PigLatin();
+    }
+
     public function testWordBeginningWithP()
     {
-        $translator = new PigLatin();
-        $this->assertEquals("igpay", $translator->translate("pig"));
+        $this->assertEquals("igpay", $this->translator->translate("pig"));
     }
 
     public function testWordBeginningWithK()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("oalakay", $translator->translate("koala"));
+
+        $this->assertEquals("oalakay", $this->translator->translate("koala"));
     }
 
     public function testWordBeginningWithY()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("ellowyay", $translator->translate("yellow"));
+
+        $this->assertEquals("ellowyay", $this->translator->translate("yellow"));
     }
 
     public function testWordBeginningWithX()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("enonxay", $translator->translate("xenon"));
+
+        $this->assertEquals("enonxay", $this->translator->translate("xenon"));
     }
 
     public function testWordBeginningWithA()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("appleay", $translator->translate("apple"));
+
+        $this->assertEquals("appleay", $this->translator->translate("apple"));
     }
 
     public function testWordBeginningWithE()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("earay", $translator->translate("ear"));
+
+        $this->assertEquals("earay", $this->translator->translate("ear"));
     }
 
     public function testWordBeginningWithI()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("iglooay", $translator->translate("igloo"));
+
+        $this->assertEquals("iglooay", $this->translator->translate("igloo"));
     }
 
     public function testWordBeginningWithO()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("objectay", $translator->translate("object"));
+
+        $this->assertEquals("objectay", $this->translator->translate("object"));
     }
 
     public function testWordBeginningWithU()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("underay", $translator->translate("under"));
+
+        $this->assertEquals("underay", $this->translator->translate("under"));
     }
 
     public function testWordBeginningVowelFollowedByQu()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("equalay", $translator->translate("equal"));
+
+        $this->assertEquals("equalay", $this->translator->translate("equal"));
     }
 
 
     public function testWordBeginningWithQWithoutAFollowingU()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("atqay", $translator->translate("qat"));
+
+        $this->assertEquals("atqay", $this->translator->translate("qat"));
     }
 
 
     public function testWordBeginningWithCh()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("airchay", $translator->translate("chair"));
+
+        $this->assertEquals("airchay", $this->translator->translate("chair"));
     }
 
     public function testWordBeginningWithQu()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("eenquay", $translator->translate("queen"));
+
+        $this->assertEquals("eenquay", $this->translator->translate("queen"));
     }
 
     public function testWordBeginningWithQuAndAPrecedingConsonant()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("aresquay", $translator->translate("square"));
+
+        $this->assertEquals("aresquay", $this->translator->translate("square"));
     }
 
     public function testWordBeginningWithTh()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("erapythay", $translator->translate("therapy"));
+
+        $this->assertEquals("erapythay", $this->translator->translate("therapy"));
     }
 
     public function testWordBeginningWithThr()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("ushthray", $translator->translate("thrush"));
+
+        $this->assertEquals("ushthray", $this->translator->translate("thrush"));
     }
 
     public function testWordBeginningWithSch()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("oolschay", $translator->translate("school"));
+
+        $this->assertEquals("oolschay", $this->translator->translate("school"));
     }
 
     public function testWordBeginningWithYt()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("yttriaay", $translator->translate("yttria"));
+
+        $this->assertEquals("yttriaay", $this->translator->translate("yttria"));
     }
 
     public function testWordBeginningWithXr()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("xrayay", $translator->translate("xray"));
+
+        $this->assertEquals("xrayay", $this->translator->translate("xray"));
     }
 
     public function testAWholePhrase()
     {
         $this->markTestSkipped();
-        $translator = new PigLatin();
-        $this->assertEquals("ickquay astfay unray", $translator->translate("quick fast run"));
+
+        $this->assertEquals("ickquay astfay unray", $this->translator->translate("quick fast run"));
     }
 }
