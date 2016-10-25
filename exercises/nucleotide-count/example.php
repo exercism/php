21ddb3324482e054e4d4820d0947d7114537ed70
mike <mike.simonson@gmail.com>
2016-10-25 23:29:19 +0200

    Adding the nucleotide count exercice

---
+++ exercises/nucleotide-count/example.php
@@ -0,0 +1,25 @@
+<?php
+
+
+function nucleotideCount($dna)
+{
+    $nucleotides = [];
+    if (strlen($dna) > 0) {
+        $nucleotides = str_split($dna);
+    }
+    $count = array_reduce($nucleotides, function ($count, $nucleotide) {
+        if (! in_array(strtolower($nucleotide), ['a', 'c', 'g', 't'])) {
+            throw new InvalidArgumentException(sprintf('The nucleotide %s does not exist in DNA', $nucleotide));
+        }
+        $count[strtolower($nucleotide)]++;
+
+        return $count;
+    }, [
+        'a' => 0,
+        'c' => 0,
+        't' => 0,
+        'g' => 0,
+    ]);
+
+    return $count;
+}
---
+++ exercises/nucleotide-count/nucleotide-count_test.php
@@ -0,0 +1,36 @@
+<?php
+
+require "nucleotide-count.php";
+
+class NucleotideCountTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEmptyDNASequence()
+    {
+        $this->assertSame([
+            'a' => 0,
+            'c' => 0,
+            't' => 0,
+            'g' => 0,
+        ], nucleotideCount(''));
+    }
+
+    public function testRepetitiveDNASequence()
+    {
+        $this->assertSame([
+            'a' => 9,
+            'c' => 0,
+            't' => 0,
+            'g' => 0,
+        ], nucleotideCount('AAAAAAAAA'));
+    }
+
+    public function testDNASequence()
+    {
+        $this->assertSame([
+            'a' => 20,
+            'c' => 12,
+            't' => 21,
+            'g' => 17,
+        ], nucleotideCount('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'));
+    }
+}
