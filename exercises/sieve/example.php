d5e89e7ee850bf4a2e905167f26731ced5d4b50e
jesuslc <jeslopcru@gmail.com>
2016-11-29 22:12:42 +0100

    Add tests and example for Sieve exercise (#136)
    * Add tests and example for Sieve exercise
    
    * use integers instead of string

---
+++ exercises/sieve/example.php
@@ -0,0 +1,29 @@
+<?php
+function sieve($number)
+{
+    if ($number == 1) {
+        return [];
+    }
+    $candidates = _getNumbersFrom2To($number);
+    foreach ($candidates as $prime) {
+        foreach ($candidates as $key => $multiple) {
+            if (_areDifferent($multiple, $prime) && _areMultiples($multiple, $prime)) {
+                unset($candidates[$key]);
+            }
+        }
+    }
+    return array_values($candidates);
+}
+function _getNumbersFrom2To($number): array
+{
+    $candidates = range(2, $number);
+    return $candidates;
+}
+function _areDifferent($multiple, $prime): bool
+{
+    return ($multiple != $prime);
+}
+function _areMultiples($multiple, $prime): bool
+{
+    return ($multiple % $prime == 0);
+}
