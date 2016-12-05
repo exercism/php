c0d52dfa56d9fbadcb198e368bbcea4bd6074369
Hrumpa <Hrumpa@users.noreply.github.com>
2016-12-05 12:34:26 +0300

    Robot-name: added name uniqueness tests for mass cases (#137)

--- exercises/robot-name/example.php
+++ exercises/robot-name/example.php
@@ -1,37 +1,76 @@
 <?php
-
 class Robot
 {
-    protected $name = null;
-
-    protected $alphabet = '';
+    private $name;
 
     public function __construct()
     {
-        $this->alphabet = array_flip(str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZ'));
+        $this->reset();
     }
 
+    /**
+     * Get Robot name
+     *
+     * @return string
+     */
     public function getName()
     {
-        if (is_null($this->name)) {
-            $this->name = sprintf('%s%s', $this->getPrefix(), $this->getSuffix());
-        }
-
         return $this->name;
     }
 
-    protected function getPrefix()
-    {
-        return implode('', array_rand($this->alphabet, 2));
-    }
-
-    protected function getSuffix()
-    {
-        return strval(rand(100, 999));
-    }
-
+    /**
+     * Reset name
+     */
     public function reset()
     {
-        $this->name = null;
+        $this->name = NamesRegistry::connect()->getNewName();
+    }
+}
+
+class NamesRegistry
+{
+    private $names = [];
+    private static $letters;
+    private static $registry;
+
+    private function __construct()
+    {
+
+    }
+
+    /**
+     * Get NamesRegistry singleton
+     *
+     * @return NamesRegistry
+     */
+    public static function connect()
+    {
+        if (empty(self::$registry)) {
+            self::$registry = new NamesRegistry();
+            self::$letters = range('A', 'Z');
+        }
+        return self::$registry;
+    }
+
+    /**
+     * Get new unique robot name
+     *
+     * @todo Names rotation not implemented. Task requires all robots
+     * to have unique names even if there are obsolete names.
+     * @todo If there are plans to use Robot::reset() more than at least 200-300k times,
+     * it should be better to generate the full list of possible names at __construct
+     * and randomly reduce it on demand.
+     * @todo No fallback in case of over 676000 names.
+     *
+     * @return string New Robot name     *
+     */
+    public function getNewName()
+    {
+        do {
+            shuffle(self::$letters);
+            $name = self::$letters[0] . self::$letters[1] . sprintf('%03d', mt_rand(0, 999));
+        } while (!empty($this->names[$name]));
+        $this->names[$name] = true;
+        return $name;
     }
 }
