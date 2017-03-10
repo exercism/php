5ab6108d0d42590457cbdeeafc77d674615823c3
Ubuntu16.04.1 VP <pierre.vandesompele@etudiant.univ-lille1.fr>
2017-03-10 23:48:50 +0100

    fix indent & example file name

---
+++ exercises/grade-school/example.php
@@ -0,0 +1,33 @@
+<?php
+
+class School
+{
+    private $database = array() ;
+
+    public function numberOfStudents()
+    {
+        return (count($this->database, COUNT_RECURSIVE) - count($this->database)) ;
+    }
+
+    public function add($student, $grade)
+    {
+        $this->database[$grade][] = $student ;
+    }
+
+    public function grade($grade)
+    {
+        return (array_key_exists($grade, $this->database) ? $this->database[$grade] : array()) ;
+    }
+
+    public function studentsByGradeAlphabetical()
+    {
+        $tmp = $this->database ;
+        ksort($tmp) ;
+
+        foreach ($tmp as $grade => $students) {
+            asort($students) ;
+            foreach ($students as $student) $res [ $grade ][] = $student ;
+        }
+        return $res ;
+    }
+}
--- exercises/grade-school/grade-school_test.php
+++ exercises/grade-school/grade-school_test.php
@@ -6,70 +6,70 @@ class GradeSchoolTest extends PHPUnit\Framework\TestCase
 {
     protected $school ;
 
-    protected function setUp(){
-      $this->school = new School() ;
+    protected function setUp()
+    {
+        $this->school = new School() ;
     }
 
     public function testNoStudent()
     {
-      $this->assertEquals(0, $this->school->numberOfStudents());
+        $this->assertEquals(0, $this->school->numberOfStudents());
     }
 
     public function testAddStudent()
     {
-      $this->markTestSkipped();
+        $this->markTestSkipped();
 
-      $this->school->add("Claire", 2);
-      $this->assertTrue(in_array("Claire",$this->school->grade(2)));
+        $this->school->add("Claire", 2);
+        $this->assertTrue(in_array("Claire", $this->school->grade(2)));
     }
 
     public function testAddStudentsinSameGrade()
     {
-      $this->markTestSkipped();
+        $this->markTestSkipped();
 
-      $this->school->add("Marc", 2);
-      $this->school->add("Virginie", 2);
-      $this->school->add("Claire", 2);
+        $this->school->add("Marc", 2);
+        $this->school->add("Virginie", 2);
+        $this->school->add("Claire", 2);
 
-      $students = $this->school->grade(2) ;
-      $this->assertEquals(count($students), 3);
-      $this->assertTrue(in_array("Claire",$students) && in_array("Marc",$students) && in_array("Virginie",$students));
+        $students = $this->school->grade(2) ;
+        $this->assertEquals(count($students), 3);
+        $this->assertTrue(in_array("Claire", $students) && in_array("Marc", $students) && in_array("Virginie", $students));
     }
 
     public function testAddStudentInDifferentGrades()
     {
-      $this->markTestSkipped();
+        $this->markTestSkipped();
 
-      $this->school->add("Marc", 3);
-      $this->school->add("Claire", 6);
+        $this->school->add("Marc", 3);
+        $this->school->add("Claire", 6);
 
-      $this->assertTrue(in_array("Marc",$this->school->grade(3)));
-      $this->assertTrue(in_array("Claire",$this->school->grade(6)));
-      $this->assertFalse(in_array("Marc",$this->school->grade(6)));
-      $this->assertFalse(in_array("Claire",$this->school->grade(3)));
+        $this->assertTrue(in_array("Marc", $this->school->grade(3)));
+        $this->assertTrue(in_array("Claire", $this->school->grade(6)));
+        $this->assertFalse(in_array("Marc", $this->school->grade(6)));
+        $this->assertFalse(in_array("Claire", $this->school->grade(3)));
     }
 
     public function testEmptyGrade()
     {
-      $this->markTestSkipped();
+        $this->markTestSkipped();
 
-      $this->assertTrue(empty($this->school->grade(1)));
+        $this->assertTrue(empty($this->school->grade(1)));
     }
 
     public function testSortSchool()
     {
-      $this->markTestSkipped();
+        $this->markTestSkipped();
 
-      $this->school->add("Marc", 5);
-      $this->school->add("Virginie", 5);
-      $this->school->add("Claire", 5);
-      $this->school->add("Mehdi", 4);
+        $this->school->add("Marc", 5);
+        $this->school->add("Virginie", 5);
+        $this->school->add("Claire", 5);
+        $this->school->add("Mehdi", 4);
 
-      $sortedStudents = array("4"=>array("Mehdi"),
+        $sortedStudents = array("4"=>array("Mehdi"),
                               	"5"=>array("Claire", "Marc", "Virginie")
-				) ;
-      $schoolStudents = $this->school->studentsByGradeAlphabetical() ;
-      $this->assertEquals($sortedStudents, $schoolStudents) ;
+                                ) ;
+        $schoolStudents = $this->school->studentsByGradeAlphabetical() ;
+        $this->assertEquals($sortedStudents, $schoolStudents) ;
     }
 }
-
