3a9f956a2fd3dcb398a7836b5014912a29aad4de
Ubuntu16.04.1 VP <pierre.vandesompele@etudiant.univ-lille1.fr>
2017-03-15 19:12:02 +0100

    example & tests

---
+++ exercises/luhn/luhn_test.php
@@ -0,0 +1,96 @@
+<?php
+
+require "luhn.php";
+
+class LuhnValidatorTest extends PHPUnit\Framework\TestCase
+{
+    public function testSingleDigitInvalid()
+    {
+        $this->assertFalse(isValid("1"));
+    }
+
+    public function testSingleZeroInvalid()
+    {
+
+
+        $this->assertFalse(isValid("0"));
+    }
+
+    public function testSimpleValidNumber()
+    {
+
+
+        $this->assertTrue(isValid("59"));
+    }
+
+    public function testSpaceHandling()
+    {
+
+
+        $this->assertTrue(isValid(" 5 9 "));
+    }
+
+    public function testValidCanadianSocialInsuranceNumber()
+    {
+        
+
+        $this->assertTrue(isValid("046 454 286"));
+    }
+
+    public function testInvalidCanadianSocialInsuranceNumber()
+    {
+
+
+        $this->assertFalse(isValid("046 454 287"));
+    }
+
+    public function testInvalidCreditCard()
+    {
+
+
+        $this->assertFalse(isValid("8273 1232 7352 0569"));
+    }
+
+    public function testNonDigitCharacterInValidStringInvalidatesTheString()
+    {
+
+
+        $this->assertFalse(isValid("046a 454 286"));
+    }
+
+    public function testThatStringContainingSymbolsIsInvalid()
+    {
+
+
+        $this->assertFalse(isValid("055£ 444$ 285"));
+    }
+
+    public function testThatStringContainingPunctuationIsInvalid()
+    {
+
+
+        $this->assertFalse(isValid("055-444-285"));
+    }
+
+    public function testSpaceAndSingleZeroIsInvalid()
+    {
+
+
+        $this->assertFalse(isValid(" 0"));
+    }
+
+    public function testMultipleZerosIsValid()
+    {
+
+
+        $this->assertTrue(isValid(" 00000"));
+    }
+
+    public function testThatDoublingNineIsHandledCorrectly()
+    {
+
+
+        $this->assertTrue(isValid("091"));
+    }
+}
+
