3a50768cb452b7bb716e7ea726a2695b0e2b1b87
Ubuntu16.04.1 VP <pierre.vandesompele@etudiant.univ-lille1.fr>
2017-03-16 14:06:13 +0100

    style fix

--- exercises/luhn/luhn_test.php
+++ exercises/luhn/luhn_test.php
@@ -11,84 +11,84 @@ class LuhnValidatorTest extends PHPUnit\Framework\TestCase
 
     public function testSingleZeroInvalid()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertFalse(isValid("0"));
     }
 
     public function testSimpleValidNumber()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertTrue(isValid("59"));
     }
 
     public function testSpaceHandling()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertTrue(isValid(" 5 9 "));
     }
 
     public function testValidCanadianSocialInsuranceNumber()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertTrue(isValid("046 454 286"));
     }
 
     public function testInvalidCanadianSocialInsuranceNumber()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertFalse(isValid("046 454 287"));
     }
 
     public function testInvalidCreditCard()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertFalse(isValid("8273 1232 7352 0569"));
     }
 
     public function testNonDigitCharacterInValidStringInvalidatesTheString()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertFalse(isValid("046a 454 286"));
     }
 
     public function testThatStringContainingSymbolsIsInvalid()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertFalse(isValid("055£ 444$ 285"));
     }
 
     public function testThatStringContainingPunctuationIsInvalid()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertFalse(isValid("055-444-285"));
     }
 
     public function testSpaceAndSingleZeroIsInvalid()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertFalse(isValid(" 0"));
     }
 
     public function testMultipleZerosIsValid()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertTrue(isValid(" 00000"));
     }
 
     public function testThatDoublingNineIsHandledCorrectly()
     {
-	$this->markTestSkipped();
+        $this->markTestSkipped();
 
         $this->assertTrue(isValid("091"));
     }
