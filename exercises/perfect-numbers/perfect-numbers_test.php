0a3d684ca6903a124854178880a1ca35ac8b7fdd
Vandesompele Pierre <vandesompele@a14p14.fil.univ-lille1.fr>
2017-03-17 14:24:51 +0100

    test file started

--- exercises/perfect-numbers/perfect-numbers_test.php
+++ exercises/perfect-numbers/perfect-numbers_test.php
@@ -4,4 +4,88 @@ require "perfect-numbers.php";
 
 class PerfectNumbersTest extends PHPUnit\Framework\TestCase
 {
+    public function testSmallPerfectNumberIsClassifiedCorrectly()
+    {
+        $this->assertEquals("perfect",getClassification(6));
+    }
+
+    public function testMediumPerfectNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertFalse("perfect",getClassification(28));
+    }
+
+    public function testLargePerfectNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("perfect",getClassification(33550336));
+    }
+
+    public function testSmallAbundantNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("abundant",getClassification(12));
+    }
+
+    public function testMediumAbundantNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("abundant",getClassification(30));
+    }
+
+    public function testLargeAbundantNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("abundant",getClassification(33550335));
+    }
+
+    public function testSmallestPrimeDeficientNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("deficient",getClassification(2));
+    }
+
+    public function testSmallestNonPrimeDeficientNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("deficient",getClassification(4));
+    }
+
+    public function testMediumDeficientNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("deficient",getClassification(32));
+    }
+
+    public function testLargeDeficientNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("deficient",getClassification(33550335));
+    }
+
+    public function testThatOneIsCorrectlyClassifiedAsDeficient()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("deficient",getClassification(1));
+    }
+
+    public function testThatNonNegativeIntegerIsRejected()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testThatNegativeIntegerIsRejected()
+    {
+        $this->markTestSkipped();
+    }
 }
