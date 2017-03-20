331fb50838f5ffc2226609cde35f119b894f6538
Pierre V <Pierre-Vdsp@users.noreply.github.com>
2017-03-20 10:50:08 +0100

    Add exercise: perfect-numbers (#150)
    * dibs on perfect-numbers
    
    * empty files
    
    * test file started
    
    * add example and finish tests
    
    * fix style
    
    * fix style for
    
    * fix style for

---
+++ exercises/perfect-numbers/example.php
@@ -0,0 +1,23 @@
+<?php
+
+function getClassification($number)
+{
+    if ($number <= 0) {
+         throw new InvalidArgumentException("You must supply a natural number (positive integer)");
+    }
+
+    $sum = 0 ;
+    for ($i = 1; $i < $number; $i++) {
+        if ($number % $i == 0) {
+            $sum += $i ;
+        }
+    }
+
+    if ($sum == $number) {
+        return "perfect" ;
+    } elseif ($sum < $number) {
+        return "deficient" ;
+    } else {
+        return "abundant" ;
+    }
+}
---
+++ exercises/perfect-numbers/perfect-numbers_test.php
@@ -0,0 +1,99 @@
+<?php
+
+require "perfect-numbers.php";
+
+class PerfectNumbersTest extends PHPUnit\Framework\TestCase
+{
+    public function testSmallPerfectNumberIsClassifiedCorrectly()
+    {
+        $this->assertEquals("perfect", getClassification(6));
+    }
+
+    public function testMediumPerfectNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("perfect", getClassification(28));
+    }
+
+    public function testLargePerfectNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("perfect", getClassification(33550336));
+    }
+
+    public function testSmallAbundantNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("abundant", getClassification(12));
+    }
+
+    public function testMediumAbundantNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("abundant", getClassification(30));
+    }
+
+    public function testLargeAbundantNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("abundant", getClassification(33550335));
+    }
+
+    public function testSmallestPrimeDeficientNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("deficient", getClassification(2));
+    }
+
+    public function testSmallestNonPrimeDeficientNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("deficient", getClassification(4));
+    }
+
+    public function testMediumDeficientNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("deficient", getClassification(32));
+    }
+
+    public function testLargeDeficientNumberIsClassifiedCorrectly()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("deficient", getClassification(33550337));
+    }
+
+    public function testThatOneIsCorrectlyClassifiedAsDeficient()
+    {
+        $this->markTestSkipped();
+
+        $this->assertEquals("deficient", getClassification(1));
+    }
+
+    public function testThatNonNegativeIntegerIsRejected()
+    {
+        $this->markTestSkipped();
+
+        $this->expectException(InvalidArgumentException::class) ;
+
+        getClassification(0) ;
+    }
+
+    public function testThatNegativeIntegerIsRejected()
+    {
+        $this->markTestSkipped();
+
+        $this->expectException(InvalidArgumentException::class) ;
+
+        getClassification(-1) ;
+    }
+}
