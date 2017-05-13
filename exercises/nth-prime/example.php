a5a13b3e03160b6866496272665e19e448d625ff
Arthur Dean <rdrr984@gmail.com>
2017-05-13 13:12:59 -0400

    Add the nth-prime exercise

---
+++ exercises/nth-prime/example.php
@@ -0,0 +1,19 @@
+<?php
+function prime($count)
+{
+    if ($count < 1) {
+        return false;
+    }
+    $primes = [];
+    $i = 2;
+    while (count($primes) < $count) {
+        foreach ($primes as $prime) {
+            if ($i % $prime == 0) {
+                $i++;
+                continue 2;
+            }
+        }
+        $primes[]=$i++;
+    }
+    return end($primes);
+}
