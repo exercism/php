773143b2c56fd84d3e8e298e8d013540ce704c42
krishna <krishwebz@gmail.com>
2017-09-11 01:44:49 +0530

    [PHP-Exercise][FlattenArray] Fix PSR standards

--- exercises/flatten-array/example.php
+++ exercises/flatten-array/example.php
@@ -3,6 +3,8 @@
 function flatten($array = [])
 {
     $return = array();
-    array_walk_recursive($array, function($x) use (&$return) { return !is_null($x) ? $return[] = $x : []; });
+    array_walk_recursive($array, function ($x) use (&$return) {
+        return !is_null($x) ? $return[] = $x : [];
+    });
     return $return;
 }
