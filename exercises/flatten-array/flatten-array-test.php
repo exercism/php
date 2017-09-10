5c9f53029a9194c32f1e1684e88ff9c608a321d8
krishna <krishwebz@gmail.com>
2017-09-11 01:47:22 +0530

    [PHP-Exercise][FlattenArray] Update skip testcases

--- exercises/flatten-array/flatten-array-test.php
+++ exercises/flatten-array/flatten-array-test.php
@@ -12,7 +12,7 @@ class FlattenArrayTest extends PHPUnit\Framework\TestCase
     }
     public function testArrayWithJustIntegersPresent()
     {
-        //$this->markTestSkipped();
+        $this->markTestSkipped();
         $input = [1, [2, 3, 4, 5, 6, 7], 8];
         $expected = [1, 2, 3, 4, 5, 6, 7, 8];
         $this->assertEquals($expected, flatten($input));
@@ -20,21 +20,22 @@ class FlattenArrayTest extends PHPUnit\Framework\TestCase
 
     public function testFiveLevelNesting()
     {
-        //$this->markTestSkipped();
+        $this->markTestSkipped();
         $input = [0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2];
         $expected = [0, 2, 2, 3, 8, 100, 4, 50, -2];
         $this->assertEquals($expected, flatten($input));
     }
+
     public function testSixLevelNesting()
     {
-        //$this->markTestSkipped();
+        $this->markTestSkipped();
         $input = [1, [2, [[3]], [4, [[5]]], 6, 7], 8];
         $expected = [1, 2, 3, 4, 5, 6, 7, 8];
         $this->assertEquals($expected, flatten($input));
     }
     public function testSixLevelNestListWithNullValues()
     {
-        //$this->markTestSkipped();
+        $this->markTestSkipped();
         $input = [0, 2, [[2, 3], 8, [[100]], null, [[null]]], -2];
         $expected = [0, 2, 2, 3, 8, 100, -2];
         $this->assertEquals($expected, flatten($input));
@@ -42,7 +43,7 @@ class FlattenArrayTest extends PHPUnit\Framework\TestCase
 
     public function testAllValuesInNestedListAreNull()
     {
-        //$this->markTestSkipped();
+        $this->markTestSkipped();
         $input = [null, [[[null]]], null, null, [[null, null], null], null];
         $expected = [];
         $this->assertEquals($expected, flatten($input));
