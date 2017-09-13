75e1569f8ba2975f5dac813f5d786af83c301abe
Krishnakumar <krishwebz@gmail.com>
2017-09-13 13:15:53 +0530

    Implements exercise flatten-array (#171)
    * Implement exercise flatten-array
    
    * [PHP-Exercise][FlattenArray] update configration for flatten array exercise php
    
    * [PHP-Exercise][FlattenArray] added test case and example func for flatten array exercise php
    
    * [PHP-Exercise][FlattenArray] added newline EOF
    
    * [PHP-Exercise][FlattenArray] Fix PSR standards
    
    * [PHP-Exercise][FlattenArray] Update skip testcases
    
    * [PHP-Exercise][FlattenArray] Renamed test case fle
    
    * [PHP-Exercise][FlattenArray] Updated require function filename
    
    * [PHP-Exercise][FlattenArray] Updated uuid into config
    
    * [PHP-Exercise][FlattenArray] removed uuid in config
    
    * [PHP-Exercise][FlattenArray] added uuid in config

---
+++ exercises/flatten-array/example.php
@@ -0,0 +1,10 @@
+<?php
+
+function flatten($array = [])
+{
+    $return = array();
+    array_walk_recursive($array, function ($x) use (&$return) {
+        return !is_null($x) ? $return[] = $x : [];
+    });
+    return $return;
+}
