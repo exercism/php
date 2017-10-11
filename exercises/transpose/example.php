c2f0682f39a234eb595a0e2216dcdd923bdafe14
Krishnakumar <krishwebz@gmail.com>
2017-10-11 19:13:03 +0000

    Implemented Transpose Exercise (#174)

---
+++ exercises/transpose/example.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Transpose multi line text into Rows become columns and columns become rows.
+ * Eg: http://en.wikipedia.org/wiki/Transpose
+ *
+ * @param String $text - Multi-line input
+ *
+ * @return string
+ */
+function transpose($text)
+{
+    $result = [];
+    if (!empty($text)) {
+        $lines = (strpos($text, '\n') !== false ? explode('\n', $text) : [$text]);
+        foreach ($lines as $lineNumber => $line) {
+            $characters = str_split($line);
+            array_walk_recursive(
+                $characters,
+                function ($character, $key) use (&$result, $lineNumber) {
+                    $defaultValue = str_pad($character, $lineNumber + 1, " ", STR_PAD_LEFT);
+                    if (isset($result[$key])) {
+                        $defaultValue = $result[$key] . $character;
+                    }
+                    $result[$key] = $defaultValue;
+                }
+            );
+        }
+    }
+    return implode($result, '\n');
+}
