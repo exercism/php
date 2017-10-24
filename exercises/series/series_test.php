4ec8c87af71f95dbc4690f9478c1510e35c9d70f
Camilo Payan <camilopayan@users.noreply.github.com>
2017-10-24 04:35:47 -0400

    Implements the series exercise (#182)
    * Dibs on the series exercise
    
    * Implements the Series exercise in PHP

---
+++ exercises/series/series_test.php
@@ -0,0 +1,64 @@
+<?php
+require "series.php";
+use Exception;
+
+class SieveTest extends PHPUnit\Framework\TestCase
+{
+    public function testSlicesOfOne()
+    {
+        $this->assertEquals(
+            slices("01234", 1),
+            ["0", "1", "2", "3", "4"]
+        );
+    }
+
+    public function testSlicesOfTwo()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(
+            slices("97867564", 2),
+            ["97", "78", "86", "67", "75", "56", "64"]
+        );
+    }
+
+    public function testSlicesOfThree()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(
+            slices("97867564", 3),
+            ["978", "786", "867", "675", "756", "564"]
+        );
+    }
+
+    public function testSlicesOfFour()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(
+            slices("01234", 4),
+            ["0123", "1234"]
+        );
+    }
+
+    public function testSlicesOfFive()
+    {
+        $this->markTestSkipped();
+        $this->assertEquals(
+            slices("01234", 5),
+            ["01234"]
+        );
+    }
+
+    public function testOverlyLongSlice()
+    {
+        $this->markTestSkipped();
+        $this->expectException(Exception::class);
+        slices("012", 4);
+    }
+
+    public function testOverlyShortSlice()
+    {
+        $this->markTestSkipped();
+        $this->expectException(Exception::class);
+        slices("01234", 0);
+    }
+}
