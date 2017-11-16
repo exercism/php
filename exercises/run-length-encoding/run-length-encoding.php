4e19eaf82ac5721559a6d3fe98db8b0f4d2ac5c5
Leprechaunz <leprechaunxx@gmail.com>
2017-11-16 15:34:17 +0200

    Implement Run Length Encoding (#188)
    * Implement Run Length Encoding
    
    * Implement Run Length Encoding
    
    * Checkstyle fixed & removed class
    
    * New UUID

---
+++ exercises/run-length-encoding/run-length-encoding.php
@@ -0,0 +1,20 @@
+<?php
+
+//
+// This is only a SKELETON file for the "Run Length Encoding" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+function encode($input)
+{
+    //
+    // YOUR CODE GOES HERE
+    //
+}
+
+function decode($input)
+{
+    //
+    // YOUR CODE GOES HERE
+    //
+}
