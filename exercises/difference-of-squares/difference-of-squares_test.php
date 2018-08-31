0dadcebdd987b3e49c273b49fb164fc215e4e22d
Bentbib Rachad <bentbibrachad@gmail.com>
2018-08-31 11:13:52 +0100

    difference-of-squares: renamed squareOfSums to squareOfSum (#215)

--- exercises/difference-of-squares/difference-of-squares_test.php
+++ exercises/difference-of-squares/difference-of-squares_test.php
@@ -4,9 +4,9 @@ require_once "difference-of-squares.php";
 
 class SquaresTest extends PHPUnit\Framework\TestCase
 {
-    public function testSquareOfSumsTo5()
+    public function testSquareOfSumTo5()
     {
-        $this->assertEquals(225, squareOfSums(5));
+        $this->assertEquals(225, squareOfSum(5));
     }
 
     public function testSumOfSquaresTo5()
@@ -14,14 +14,14 @@ class SquaresTest extends PHPUnit\Framework\TestCase
         $this->assertEquals(55, sumOfSquares(5));
     }
 
-    public function testDifferenceOfSumsTo5()
+    public function testDifferenceOfSumTo5()
     {
         $this->assertEquals(170, difference(5));
     }
 
-    public function testSquareOfSumsTo10()
+    public function testSquareOfSumTo10()
     {
-        $this->assertEquals(3025, squareOfSums(10));
+        $this->assertEquals(3025, squareOfSum(10));
     }
 
     public function testSumOfSquaresTo10()
@@ -29,14 +29,14 @@ class SquaresTest extends PHPUnit\Framework\TestCase
         $this->assertEquals(385, sumOfSquares(10));
     }
 
-    public function testDifferenceOfSumsTo10()
+    public function testDifferenceOfSumTo10()
     {
         $this->assertEquals(2640, difference(10));
     }
 
-    public function testSquareOfSumsTo100()
+    public function testSquareOfSumTo100()
     {
-        $this->assertEquals(25502500, squareOfSums(100));
+        $this->assertEquals(25502500, squareOfSum(100));
     }
 
     public function testSumOfSquaresTo100()
@@ -44,7 +44,7 @@ class SquaresTest extends PHPUnit\Framework\TestCase
         $this->assertEquals(338350, sumOfSquares(100));
     }
 
-    public function testDifferenceOfSumsTo100()
+    public function testDifferenceOfSumTo100()
     {
         $this->assertEquals(25164150, difference(100));
     }
