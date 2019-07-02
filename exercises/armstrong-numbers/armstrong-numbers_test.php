b26ea71f43848d18e9e442c735018ffd4ed990bb
Anthony Scotti <amscotti@users.noreply.github.com>
2019-07-02 06:42:36 -0400

    Adds armstrong-numbers exercise (#259)

---
+++ exercises/armstrong-numbers/armstrong-numbers_test.php
@@ -0,0 +1,51 @@
+<?php
+
+require "armstrong-numbers.php";
+
+class ArmstrongNumbersTest extends PHPUnit\Framework\TestCase
+{
+    public function testZero()
+    {
+        $this->assertTrue(isArmstrongNumber(0));
+    }
+
+    public function testSingleDigit()
+    {
+        $this->assertTrue(isArmstrongNumber(5));
+    }
+
+    public function testDoubleDigit()
+    {
+        $this->assertFalse(isArmstrongNumber(10));
+    }
+
+    public function testThreeDigitIsArmstrongNumber()
+    {
+        $this->assertTrue(isArmstrongNumber(153));
+    }
+
+    public function testThreeDigitIsNotArmstrongNumber()
+    {
+        $this->assertFalse(isArmstrongNumber(100));
+    }
+
+    public function testFourDigitIsArmstrongNumber()
+    {
+        $this->assertTrue(isArmstrongNumber(9474));
+    }
+
+    public function testFourDigitIsNotArmstrongNumber()
+    {
+        $this->assertFalse(isArmstrongNumber(9475));
+    }
+
+    public function testSevenDigitIsArmstrongNumber()
+    {
+        $this->assertTrue(isArmstrongNumber(9926315));
+    }
+
+    public function testSevenDigitIsNotArmstrongNumber()
+    {
+        $this->assertFalse(isArmstrongNumber(9926314));
+    }
+}
