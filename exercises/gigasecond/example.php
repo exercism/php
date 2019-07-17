4fed1a772bdd874199fa9f86b19a32f290132d88
Andi Rückauer <arueckauer@github.com>
2019-07-17 08:53:12 +0200

    Improve example by removing unnecessary clone of DateTimeImmutable object

--- exercises/gigasecond/example.php
+++ exercises/gigasecond/example.php
@@ -3,6 +3,5 @@
 function from(DateTimeImmutable $from)
 {
     $interval = new DateInterval('PT1000000000S');
-    $date = clone $from;
-    return $date->add($interval);
+    return $from->add($interval);
 }
