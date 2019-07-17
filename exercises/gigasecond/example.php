129df8cc142594b1a1ebd6dbe35230069454eace
Andi Rückauer <arueckauer@github.com>
2019-07-17 08:53:51 +0200

    Declare return type

--- exercises/gigasecond/example.php
+++ exercises/gigasecond/example.php
@@ -1,6 +1,6 @@
 <?php
 
-function from(DateTimeImmutable $from)
+function from(DateTimeImmutable $from) : DateTimeImmutable
 {
     $interval = new DateInterval('PT1000000000S');
     return $from->add($interval);
