50e50d4829a3a084d1fd23abcd4a29cce5b4f54d
Andi Rückauer <arueckauer@github.com>
2019-07-25 11:36:29 +0200

    Update example to match test cases

--- exercises/phone-number/example.php
+++ exercises/phone-number/example.php
@@ -19,27 +19,32 @@ class PhoneNumber
         return substr($this->number, 0, 3);
     }
 
-    public function prettyPrint(): string
+    public function prettyPrint() : string
     {
         return '(' . $this->areaCode() . ') ' . $this->prefix() . '-' . $this->lineNumber();
     }
 
-    private function clean($number) : string
+    private function clean(string $number) : string
     {
-        return $this->validate(preg_replace('/[^0-9a-z]+/i', '', $number));
+        return $this->validate(preg_replace('/[^0-9a-z@:!]+/i', '', $number));
     }
 
-    private function validate($number) : string
+    private function validate(string $number) : string
     {
-        if ($number !== preg_replace('/[^0-9]+/', '', $number)) {
-            throw new InvalidArgumentException('Given number contains invalid characters.');
+        if (strlen($number) === 11 && $number[0] !== '1') {
+            throw new InvalidArgumentException('11 digits must start with 1');
         }
 
-        $result = preg_match('/\(?[2-9][0-9]{2}\)?[-. ]?[2-9][0-9]{2}[-. ]?[0-9]{4}/', $number);
-        if (1 !== $result) {
-            throw new InvalidArgumentException(
-                'Given number has an invalid format.' . PHP_EOL . $number . PHP_EOL . $result
-            );
+        if (strlen($number) > 11) {
+            throw new InvalidArgumentException('more than 11 digits');
+        }
+
+        if (0 !== preg_match('/[A-z]/', $number)) {
+            throw new InvalidArgumentException('letters not permitted');
+        }
+
+        if (0 !== preg_match('/[@:!]/', $number)) {
+            throw new InvalidArgumentException('punctuations not permitted');
         }
 
         if (strlen($number) === 11) {
@@ -47,7 +52,23 @@ class PhoneNumber
         }
 
         if (strlen($number) !== 10) {
-            throw new InvalidArgumentException('Given number has an invalid length.');
+            throw new InvalidArgumentException('incorrect number of digits');
+        }
+
+        if ($number[0] === '0') {
+            throw new InvalidArgumentException('area code cannot start with zero');
+        }
+
+        if ($number[0] === '1') {
+            throw new InvalidArgumentException('area code cannot start with one');
+        }
+
+        if ($number[3] === '0') {
+            throw new InvalidArgumentException('exchange code cannot start with zero');
+        }
+
+        if ($number[3] === '1') {
+            throw new InvalidArgumentException('exchange code cannot start with one');
         }
 
         return $number;
