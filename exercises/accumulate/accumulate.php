330f6d31b73005783b60adbfb7c3b9b765cf8c16
Andi Rückauer <arueckauer@github.com>
2019-08-05 13:12:35 +0200

    Fix code style by removing double line feed

--- exercises/accumulate/accumulate.php
+++ exercises/accumulate/accumulate.php
@@ -1,6 +1,1 @@
 <?php
-
-function accumulate(array $input, callable $accumulator)
-{
-    // YOUR CODE GOES HERE
-}
--- exercises/hello-world/hello-world.php
+++ exercises/hello-world/hello-world.php
@@ -1,13 +1,1 @@
 <?php
-
-//
-// This is only a SKELETON file for the "Hello World" exercise.
-// It's been provided as a convenience to get you started writing code faster.
-//
-
-function helloWorld()
-{
-    //
-    // YOUR CODE GOES HERE
-    //
-}
