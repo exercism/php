eac478e77771fa607783fae8a2e7e6756b557803
Tim Austin <tim@neenjaw.com>
2021-02-04 06:39:11 -0600

    Fix deprecated string indexing in luhn exercise (#333)

--- exercises/practice/luhn/example.php
+++ exercises/practice/luhn/example.php
@@ -12,14 +12,14 @@ function isValid($candidate)
     $sum = 0 ;
 
     for ($i = 1; $i < strlen($reversseCandidate); $i += 2) {
-        $digit = 2 * intval($reversseCandidate{$i}) ;
+        $digit = 2 * intval($reversseCandidate[$i]) ;
 
         if ($digit > 9) {
             $digit -= 9 ;
         }
 
         $sum += $digit ;
-        $sum += intval($reversseCandidate{$i - 1}) ;
+        $sum += intval($reversseCandidate[$i - 1]) ;
     }
 
     return $sum % 10 == 0 ;
