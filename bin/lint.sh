3132c4991c94e6b1422ef517cebb1201769828fe
Tim Austin <tim@neenjaw.com>
2021-02-06 13:33:59 -0600

    all exercises now tested before the makefile/script used to stop after the first error, now it runs against all.

--- bin/lint.sh
+++ bin/lint.sh
@@ -5,13 +5,18 @@ set -euo pipefail
 function main {
   all_exercise_dirs=$(find ./exercises -maxdepth 2 -mindepth 2 -type d | sort)
   for exercise_dir in $all_exercise_dirs; do
-    exercise=$(basename "${exercise_dir}")
-    echo "checking ${exercise} against PHP code standards"
-
-    eval "${PHPCS_BIN}" -sp --standard="${PHPCS_RULES}" "${exercise_dir}"
+    lint "${exercise_dir}" || true
   done
 
   return 0
+}
+
+function lint {
+  exercise_dir="${1}"
+  exercise=$(basename "${exercise_dir}")
+  echo "checking ${exercise} against PHP code standards"
+
+  eval "${PHPCS_BIN}" -sp --standard="${PHPCS_RULES}" "${exercise_dir}"
 }
 
 function installed {
@@ -27,7 +32,7 @@ function die {
 }
 
 if [[ -z "${PHPCS_BIN:-}" ]]; then
-  die "Missing PHPUNIT_BIN environment variable";
+  die "Missing PHPCS_BIN environment variable";
 fi
 
 if [[ -z "${PHPCS_RULES:-}" ]]; then
--- bin/test.sh
+++ bin/test.sh
@@ -8,19 +8,24 @@ file_ext="php"
 function main {
   all_exercise_dirs=$(find ./exercises -maxdepth 2 -mindepth 2 -type d | sort)
   for exercise_dir in $all_exercise_dirs; do
-    exercise=$(basename "${exercise_dir}")
-    echo "running tests for: ${exercise}"
-
-    example_file="example.${file_ext}"
-    test_file="${exercise}_test.${file_ext}"
-    outdir=$(mktemp -d "${tmpdir}/${exercise}.XXXXXXXXXX")
-
-    cat "${exercise_dir}/${test_file}" | sed '/markTestSkipped()/d' > "${outdir}/${test_file}"
-    cp "${exercise_dir}/${example_file}" "${outdir}/${exercise}.${file_ext}"
-    eval "${PHPUNIT_BIN}" --no-configuration "${outdir}/${test_file}"
+    test "${exercise_dir}" || true
   done
 
   return 0
+}
+
+function test {
+  exercise_dir="${1}"
+  exercise=$(basename "${exercise_dir}")
+  echo "running tests for: ${exercise}"
+
+  example_file="example.${file_ext}"
+  test_file="${exercise}_test.${file_ext}"
+  outdir=$(mktemp -d "${tmpdir}/${exercise}.XXXXXXXXXX")
+
+  cat "${exercise_dir}/${test_file}" | sed '/markTestSkipped()/d' > "${outdir}/${test_file}"
+  cp "${exercise_dir}/${example_file}" "${outdir}/${exercise}.${file_ext}"
+  eval "${PHPUNIT_BIN}" --no-configuration "${outdir}/${test_file}"
 }
 
 function installed {
