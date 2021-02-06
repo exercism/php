f9f6645d5d15026f2351f30108d8e396701311b2
Tim Austin <tim@neenjaw.com>
2021-02-06 12:33:10 -0600

    add linting

--- bin/lint.sh
+++ bin/lint.sh
@@ -3,10 +3,12 @@
 set -euo pipefail
 
 function main {
-  all_exercise_dirs=$(find ./exercises -maxdepth 2 -mindepth 2 -type d | awk -F/ '{print $NF}' | sort)
+  all_exercise_dirs=$(find ./exercises -maxdepth 2 -mindepth 2 -type d | sort)
   for exercise_dir in $all_exercise_dirs; do
     exercise=$(basename "${exercise_dir}")
-    echo "${exercise}"
+    echo "checking ${exercise} against PHP code standards"
+
+    eval "${PHPCS_BIN}" -sp --standard="${PHPCS_RULES}" "${exercise_dir}"
   done
 
   return 0
