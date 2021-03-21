97aa9c68f91b3a1ca3335c0d1201f90d8a9143c2
Holger Lösken <hl@inquence.com>
2021-03-21 11:18:42 +0100

    Add exception for empty function

--- exercises/practice/ordinal-number/OrdinalNumber.php
+++ exercises/practice/ordinal-number/OrdinalNumber.php
@@ -4,7 +4,5 @@ declare(strict_types=1);
 
 function toOrdinal(int $number): string
 {
-    //
-    // YOUR CODE GOES HERE
-    //
+    throw new \BadFunctionCallException("Implement the toOrdinal function");
 }
