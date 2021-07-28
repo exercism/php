6029712e228dd4f2fc588b27602adc1b919aa6d7
Bunker <michaelbunker1@gmail.com>
2021-07-27 21:58:02 -0700

    Cleanup resistor-color exercise files for new structure requirements.

--- exercises/resistor-color/example.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
-
-function colorCode($color)
-{
-    return array_search($color, COLORS);
-}
