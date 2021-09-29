36466f2a2bd1eb7058ccfb886cd40a79a4138f9a
David Stockton <github@davidstockton.com>
2021-09-28 22:08:26 -0600

    Fix missing newline conversion (#415)
    Co-authored-by: David Stockton <dave@davidstockton.com>

--- exercises/practice/tournament/TournamentTest.php
+++ exercises/practice/tournament/TournamentTest.php
@@ -49,9 +49,9 @@ class TournamentTest extends PHPUnit\Framework\TestCase
     {
         $scores = 'Allegoric Alaskans;Blithering Badgers;win';
         $expected =
-            'Team                           | MP |  W |  D |  L |  P\n' .
-            'Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3\n' .
-            'Blithering Badgers             |  1 |  0 |  0 |  1 |  0';
+            "Team                           | MP |  W |  D |  L |  P\n" .
+            "Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3\n" .
+            "Blithering Badgers             |  1 |  0 |  0 |  1 |  0";
         $this->assertEquals($expected, $this->tournament->tally($scores));
     }
 
@@ -59,9 +59,9 @@ class TournamentTest extends PHPUnit\Framework\TestCase
     {
         $scores = 'Blithering Badgers;Allegoric Alaskans;loss';
         $expected =
-            'Team                           | MP |  W |  D |  L |  P\n' .
-            'Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3\n' .
-            'Blithering Badgers             |  1 |  0 |  0 |  1 |  0';
+            "Team                           | MP |  W |  D |  L |  P\n" .
+            "Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3\n" .
+            "Blithering Badgers             |  1 |  0 |  0 |  1 |  0";
         $this->assertEquals($expected, $this->tournament->tally($scores));
     }
 
@@ -69,9 +69,9 @@ class TournamentTest extends PHPUnit\Framework\TestCase
     {
         $scores = 'Blithering Badgers;Allegoric Alaskans;win';
         $expected =
-            'Team                           | MP |  W |  D |  L |  P\n' .
-            'Blithering Badgers             |  1 |  1 |  0 |  0 |  3\n' .
-            'Allegoric Alaskans             |  1 |  0 |  0 |  1 |  0';
+            "Team                           | MP |  W |  D |  L |  P\n" .
+            "Blithering Badgers             |  1 |  1 |  0 |  0 |  3\n" .
+            "Allegoric Alaskans             |  1 |  0 |  0 |  1 |  0";
         $this->assertEquals($expected, $this->tournament->tally($scores));
     }
 
@@ -79,99 +79,99 @@ class TournamentTest extends PHPUnit\Framework\TestCase
     {
         $scores = 'Allegoric Alaskans;Blithering Badgers;draw';
         $expected =
-            'Team                           | MP |  W |  D |  L |  P\n' .
-            'Allegoric Alaskans             |  1 |  0 |  1 |  0 |  1\n' .
-            'Blithering Badgers             |  1 |  0 |  1 |  0 |  1';
+            "Team                           | MP |  W |  D |  L |  P\n" .
+            "Allegoric Alaskans             |  1 |  0 |  1 |  0 |  1\n" .
+            "Blithering Badgers             |  1 |  0 |  1 |  0 |  1";
         $this->assertEquals($expected, $this->tournament->tally($scores));
     }
 
     public function testThereCanBeMultipleMatches(): void
     {
         $scores =
-            'Allegoric Alaskans;Blithering Badgers;win\n' .
-            'Allegoric Alaskans;Blithering Badgers;win';
+            "Allegoric Alaskans;Blithering Badgers;win\n" .
+            "Allegoric Alaskans;Blithering Badgers;win";
         $expected =
-            'Team                           | MP |  W |  D |  L |  P\n' .
-            'Allegoric Alaskans             |  2 |  2 |  0 |  0 |  6\n' .
-            'Blithering Badgers             |  2 |  0 |  0 |  2 |  0';
+            "Team                           | MP |  W |  D |  L |  P\n" .
+            "Allegoric Alaskans             |  2 |  2 |  0 |  0 |  6\n" .
+            "Blithering Badgers             |  2 |  0 |  0 |  2 |  0";
         $this->assertEquals($expected, $this->tournament->tally($scores));
     }
 
     public function testThereCanBeMoreThanOneWinner(): void
     {
         $scores =
-            'Allegoric Alaskans;Blithering Badgers;loss\n' .
-            'Allegoric Alaskans;Blithering Badgers;win';
+            "Allegoric Alaskans;Blithering Badgers;loss\n" .
+            "Allegoric Alaskans;Blithering Badgers;win";
         $expected =
-            'Team                           | MP |  W |  D |  L |  P\n' .
-            'Allegoric Alaskans             |  2 |  1 |  0 |  1 |  3\n' .
-            'Blithering Badgers             |  2 |  1 |  0 |  1 |  3';
+            "Team                           | MP |  W |  D |  L |  P\n" .
+            "Allegoric Alaskans             |  2 |  1 |  0 |  1 |  3\n" .
+            "Blithering Badgers             |  2 |  1 |  0 |  1 |  3";
         $this->assertEquals($expected, $this->tournament->tally($scores));
     }
 
     public function testThereCanBeMoreThanTwoTeams(): void
     {
         $scores =
-            'Allegoric Alaskans;Blithering Badgers;win\n' .
-            'Blithering Badgers;Courageous Californians;win\n' .
-            'Courageous Californians;Allegoric Alaskans;loss';
+            "Allegoric Alaskans;Blithering Badgers;win\n" .
+            "Blithering Badgers;Courageous Californians;win\n" .
+            "Courageous Californians;Allegoric Alaskans;loss";
         $expected =
-            'Team                           | MP |  W |  D |  L |  P\n' .
-            'Allegoric Alaskans             |  2 |  2 |  0 |  0 |  6\n' .
-            'Blithering Badgers             |  2 |  1 |  0 |  1 |  3\n' .
-            'Courageous Californians        |  2 |  0 |  0 |  2 |  0';
+            "Team                           | MP |  W |  D |  L |  P\n" .
+            "Allegoric Alaskans             |  2 |  2 |  0 |  0 |  6\n" .
+            "Blithering Badgers             |  2 |  1 |  0 |  1 |  3\n" .
+            "Courageous Californians        |  2 |  0 |  0 |  2 |  0";
         $this->assertEquals($expected, $this->tournament->tally($scores));
     }
 
     public function testStandardInput(): void
     {
         $scores =
-            'Allegoric Alaskans;Blithering Badgers;win\n' .
-            'Devastating Donkeys;Courageous Californians;draw\n' .
-            'Devastating Donkeys;Allegoric Alaskans;win\n' .
-            'Courageous Californians;Blithering Badgers;loss\n' .
-            'Blithering Badgers;Devastating Donkeys;loss\n' .
-            'Allegoric Alaskans;Courageous Californians;win';
+            "Allegoric Alaskans;Blithering Badgers;win\n" .
+            "Devastating Donkeys;Courageous Californians;draw\n" .
+            "Devastating Donkeys;Allegoric Alaskans;win\n" .
+            "Courageous Californians;Blithering Badgers;loss\n" .
+            "Blithering Badgers;Devastating Donkeys;loss\n" .
+            "Allegoric Alaskans;Courageous Californians;win";
         $expected =
-            'Team                           | MP |  W |  D |  L |  P\n' .
-            'Devastating Donkeys            |  3 |  2 |  1 |  0 |  7\n' .
-            'Allegoric Alaskans             |  3 |  2 |  0 |  1 |  6\n' .
-            'Blithering Badgers             |  3 |  1 |  0 |  2 |  3\n' .
-            'Courageous Californians        |  3 |  0 |  1 |  2 |  1';
+            "Team                           | MP |  W |  D |  L |  P\n" .
+            "Devastating Donkeys            |  3 |  2 |  1 |  0 |  7\n" .
+            "Allegoric Alaskans             |  3 |  2 |  0 |  1 |  6\n" .
+            "Blithering Badgers             |  3 |  1 |  0 |  2 |  3\n" .
+            "Courageous Californians        |  3 |  0 |  1 |  2 |  1";
         $this->assertEquals($expected, $this->tournament->tally($scores));
     }
 
     public function testIncompleteCompetitionWhereNotAllGamesPlayed(): void
     {
         $scores =
-            'Allegoric Alaskans;Blithering Badgers;loss\n' .
-            'Devastating Donkeys;Allegoric Alaskans;loss\n' .
-            'Courageous Californians;Blithering Badgers;draw\n' .
-            'Allegoric Alaskans;Courageous Californians;win';
+            "Allegoric Alaskans;Blithering Badgers;loss\n" .
+            "Devastating Donkeys;Allegoric Alaskans;loss\n" .
+            "Courageous Californians;Blithering Badgers;draw\n" .
+            "Allegoric Alaskans;Courageous Californians;win";
         $expected =
-            'Team                           | MP |  W |  D |  L |  P\n' .
-            'Allegoric Alaskans             |  3 |  2 |  0 |  1 |  6\n' .
-            'Blithering Badgers             |  2 |  1 |  1 |  0 |  4\n' .
-            'Courageous Californians        |  2 |  0 |  1 |  1 |  1\n' .
-            'Devastating Donkeys            |  1 |  0 |  0 |  1 |  0';
+            "Team                           | MP |  W |  D |  L |  P\n" .
+            "Allegoric Alaskans             |  3 |  2 |  0 |  1 |  6\n" .
+            "Blithering Badgers             |  2 |  1 |  1 |  0 |  4\n" .
+            "Courageous Californians        |  2 |  0 |  1 |  1 |  1\n" .
+            "Devastating Donkeys            |  1 |  0 |  0 |  1 |  0";
         $this->assertEquals($expected, $this->tournament->tally($scores));
     }
 
     public function testTiesSortedAlphabetically(): void
     {
         $scores =
-            'Courageous Californians;Devastating Donkeys;win\n' .
-            'Allegoric Alaskans;Blithering Badgers;win\n' .
-            'Devastating Donkeys;Allegoric Alaskans;loss\n' .
-            'Courageous Californians;Blithering Badgers;win\n' .
-            'Blithering Badgers;Devastating Donkeys;draw\n' .
-            'Allegoric Alaskans;Courageous Californians;draw';
+            "Courageous Californians;Devastating Donkeys;win\n" .
+            "Allegoric Alaskans;Blithering Badgers;win\n" .
+            "Devastating Donkeys;Allegoric Alaskans;loss\n" .
+            "Courageous Californians;Blithering Badgers;win\n" .
+            "Blithering Badgers;Devastating Donkeys;draw\n" .
+            "Allegoric Alaskans;Courageous Californians;draw";
         $expected =
-            'Team                           | MP |  W |  D |  L |  P\n' .
-            'Allegoric Alaskans             |  3 |  2 |  1 |  0 |  7\n' .
-            'Courageous Californians        |  3 |  2 |  1 |  0 |  7\n' .
-            'Blithering Badgers             |  3 |  0 |  1 |  2 |  1\n' .
-            'Devastating Donkeys            |  3 |  0 |  1 |  2 |  1';
+            "Team                           | MP |  W |  D |  L |  P\n" .
+            "Allegoric Alaskans             |  3 |  2 |  1 |  0 |  7\n" .
+            "Courageous Californians        |  3 |  2 |  1 |  0 |  7\n" .
+            "Blithering Badgers             |  3 |  0 |  1 |  2 |  1\n" .
+            "Devastating Donkeys            |  3 |  0 |  1 |  2 |  1";
         $this->assertEquals($expected, $this->tournament->tally($scores));
     }
 }
