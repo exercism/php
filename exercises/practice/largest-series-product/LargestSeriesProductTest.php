0e98011f131979c80b65bbee703bc4cecdc529ea
Katrina Owen <katrina.owen@gmail.com>
2023-05-16 10:36:26 +0200

    Sync largest-series-product docs with problem-specifications (#509)
    * Sync largest-series-product docs with problem-specifications
    
    The largest-series-product exercise has been overhauled as part of a project
    to make practice exercises more consistent and friendly.
    
    For more context, please see the discussion in the forum, as well as
    the pull request that updated the exercise in the problem-specifications
    repository:
    
    - https://forum.exercism.org/t/new-project-making-practice-exercises-more-consistent-and-human-across-exercism/3943
    - https://github.com/exercism/problem-specifications/pull/2246
    
    * Delete test cases from largest-series-product
    
    This deletes two deprecated test cases so that we can
    dramatically simplify the instructions for this exercise.

--- exercises/practice/largest-series-product/LargestSeriesProductTest.php
+++ exercises/practice/largest-series-product/LargestSeriesProductTest.php
@@ -118,37 +118,6 @@ class LargestSeriesProductTest extends PHPUnit\Framework\TestCase
         $series->largestProduct(4);
     }
 
-    /**
-     * There may be some confusion about whether this should be 1 or error.
-     * The reasoning for it being 1 is this:
-     * There is one 0-character string contained in the empty string.
-     * That's the empty string itself.
-     * The empty product is 1 (the identity for multiplication).
-     * Therefore LSP('', 0) is 1.
-     * It's NOT the case that LSP('', 0) takes max of an empty list.
-     * So there is no error.
-     * Compare against LSP('123', 4):
-     * There are zero 4-character strings in '123'.
-     * So LSP('123', 4) really DOES take the max of an empty list.
-     * So LSP('123', 4) errors and LSP('', 0) does NOT
-     *
-     */
-    public function testReports1ForEmptyStringAndEmptyProduct0Span(): void
-    {
-        $series = new Series("");
-        $this->assertEquals(1, $series->largestProduct(0));
-    }
-
-    /**
-     * As above, there is one 0-character string in '123'.
-     * So again no error. It's the empty product, 1.
-     */
-    public function testReports1ForNonemptyStringAndEmptyProduct0Span(): void
-    {
-        $series = new Series("123");
-        $this->assertEquals(1, $series->largestProduct(0));
-    }
-
     public function testRejectsEmptyStringAndNonzeroSpan(): void
     {
         $this->expectException(InvalidArgumentException::class);
