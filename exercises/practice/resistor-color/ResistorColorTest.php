9c4d0397fcf327c7d5b27668dd6d10549ff3f971
Amar H <amar.hajrovic@m-trak.com>
2023-06-01 15:35:58 +0200

    [Fix] testColors to green

--- exercises/practice/resistor-color/ResistorColorTest.php
+++ exercises/practice/resistor-color/ResistorColorTest.php
@@ -34,16 +34,16 @@ class ResistorColorTest extends PHPUnit\Framework\TestCase
     public function testColors(): void
     {
         $this->assertEquals([
-            "black",
-            "brown",
-            "red",
-            "orange",
-            "yellow",
-            "green",
-            "blue",
-            "violet",
-            "grey",
-            "white"
+            'black' => 0,
+            'brown' => 1,
+            'red' => 2,
+            'orange' => 3,
+            'yellow' => 4,
+            'green' => 5,
+            'blue' => 6,
+            'violet' => 7,
+            'grey' => 8,
+            'white' => 9,
         ], COLORS);
     }
 
