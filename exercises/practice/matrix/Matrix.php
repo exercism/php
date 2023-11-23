38a127218b423513d2a405b7cfaeebd8e569d37e
Lucian di RinascITALIA <lucian.preda@gmail.com>
2023-11-23 15:55:47 +0100

    Add requested methods to Matrix.php template (#586)
    When offering a template, it should contain the expected signatures of the methods.
    
    [no important files changed]

--- exercises/practice/matrix/Matrix.php
+++ exercises/practice/matrix/Matrix.php
@@ -26,8 +26,18 @@ declare(strict_types=1);
 
 class Matrix
 {
-    public function __construct()
+    public function __construct(string $matrix)
     {
         throw new BadFunctionCallException("Please implement the Matrix class!");
+    }
+
+    public function getRow(int $rowId): array
+    {
+        throw new BadFunctionCallException("Please implement the getRow method!");
+    }
+
+    public function getColumn(int $columnId): array
+    {
+        throw new BadFunctionCallException("Please implement the getColumn method!");
     }
 }
