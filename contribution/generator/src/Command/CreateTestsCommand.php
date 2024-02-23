d13002cd8353115f0fe71e70cec6d5d03cf80ecc
Michael Kramer <m.kramer@mxp.de>
2024-02-23 23:46:46 +0100

    Sort `use` by alphabet

--- contribution/generator/src/Command/CreateTestsCommand.php
+++ contribution/generator/src/Command/CreateTestsCommand.php
@@ -3,15 +3,15 @@
 namespace App\Command;
 
 use PhpParser\BuilderFactory;
-use Symfony\Component\Console\Attribute\AsCommand;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use PhpParser\Node;
 use PhpParser\Node\Stmt\Namespace_;
 use PhpParser\PrettyPrinter;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:create-tests',
