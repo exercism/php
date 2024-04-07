5ac53826df796c5ec09624ba43a2eb6ed4d9e83b
Michael Kramer <m.kramer@mxp.de>
2024-04-07 17:58:50 +0200

    Order tests to match problem spec grouping

--- exercises/practice/difference-of-squares/DifferenceOfSquaresTest.php
+++ exercises/practice/difference-of-squares/DifferenceOfSquaresTest.php
@@ -10,33 +10,6 @@ class DifferenceOfSquaresTest extends PHPUnit\Framework\TestCase
     }
 
     /**
-     * uuid 9b3f96cb-638d-41ee-99b7-b4f9c0622948
-     * @testdox Square of sum 5
-     */
-    public function testSquareOfSumTo5(): void
-    {
-        $this->assertEquals(225, squareOfSum(5));
-    }
-
-    /**
-     * uuid c93900cd-8cc2-4ca4-917b-dd3027023499
-     * @testdox Sum of squares 5
-     */
-    public function testSumOfSquaresTo5(): void
-    {
-        $this->assertEquals(55, sumOfSquares(5));
-    }
-
-    /**
-     * uuid 005cb2bf-a0c8-46f3-ae25-924029f8b00b
-     * @testdox Difference of squares 5
-     */
-    public function testDifferenceOfSumTo5(): void
-    {
-        $this->assertEquals(170, difference(5));
-    }
-
-    /**
      * uuid e46c542b-31fc-4506-bcae-6b62b3268537
      * @testdox Square of sum 1
      */
@@ -46,21 +19,12 @@ class DifferenceOfSquaresTest extends PHPUnit\Framework\TestCase
     }
 
     /**
-     * uuid 01d84507-b03e-4238-9395-dd61d03074b5
-     * @testdox Sum of squares 1
+     * uuid 9b3f96cb-638d-41ee-99b7-b4f9c0622948
+     * @testdox Square of sum 5
      */
-    public function testSumOfSquaresTo1(): void
+    public function testSquareOfSumTo5(): void
     {
-        $this->assertEquals(1, sumOfSquares(1));
-    }
-
-    /**
-     * uuid 44f72ae6-31a7-437f-858d-2c0837adabb6
-     * @testdox Difference of squares 1
-     */
-    public function testDifferenceOfSumTo1(): void
-    {
-        $this->assertEquals(0, difference(1));
+        $this->assertEquals(225, squareOfSum(5));
     }
 
     /**
@@ -73,12 +37,48 @@ class DifferenceOfSquaresTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * uuid 01d84507-b03e-4238-9395-dd61d03074b5
+     * @testdox Sum of squares 1
+     */
+    public function testSumOfSquaresTo1(): void
+    {
+        $this->assertEquals(1, sumOfSquares(1));
+    }
+
+    /**
+     * uuid c93900cd-8cc2-4ca4-917b-dd3027023499
+     * @testdox Sum of squares 5
+     */
+    public function testSumOfSquaresTo5(): void
+    {
+        $this->assertEquals(55, sumOfSquares(5));
+    }
+
+    /**
      * uuid 94807386-73e4-4d9e-8dec-69eb135b19e4
      * @testdox Sum of squares 100
      */
     public function testSumOfSquaresTo100(): void
     {
         $this->assertEquals(338350, sumOfSquares(100));
+    }
+
+    /**
+     * uuid 44f72ae6-31a7-437f-858d-2c0837adabb6
+     * @testdox Difference of squares 1
+     */
+    public function testDifferenceOfSumTo1(): void
+    {
+        $this->assertEquals(0, difference(1));
+    }
+
+    /**
+     * uuid 005cb2bf-a0c8-46f3-ae25-924029f8b00b
+     * @testdox Difference of squares 5
+     */
+    public function testDifferenceOfSumTo5(): void
+    {
+        $this->assertEquals(170, difference(5));
     }
 
     /**
