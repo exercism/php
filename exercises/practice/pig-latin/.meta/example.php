06178b049fe5d434bbebe64bc00db57e660228b3
mk-mxp <55182845+mk-mxp@users.noreply.github.com>
2024-05-19 18:26:49 +0200

    Sync pig-latin (#719)

--- exercises/practice/pig-latin/.meta/example.php
+++ exercises/practice/pig-latin/.meta/example.php
@@ -1,57 +1,23 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
-/**
- * Translates a string into Pig Latin
- *
- * @param  string $str
- * @return string
- */
-function translate($str)
+function translate(string $text): string
 {
-    $words = explode(" ", $str);
+    $words = explode(" ", $text);
     $translatedWords = array_map('translateWord', $words);
     return implode(' ', $translatedWords);
 }
 
-/**
- * Translates a single word
- *
- * @param  string $str
- * @return string
- */
-function translateWord($str)
+function translateWord(string $text): string
 {
-    if (preg_match('/^s?qu|thr?|s?ch/', $str, $match)) {
-        return sprintf("%s%say", substr($str, strlen($match[0])), $match[0]);
+    if (preg_match('/^s?qu|thr?|s?ch|rh/', $text, $match)) {
+        return sprintf("%s%say", substr($text, strlen($match[0])), $match[0]);
     }
 
-    if (preg_match('/^[aeiou]|yt|xr/', $str)) {
-        return sprintf("%say", $str);
+    if (preg_match('/^[aeiou]|yt|xr/', $text)) {
+        return sprintf("%say", $text);
     }
 
-    return sprintf("%s%say", substr($str, 1), $str[0]);
+    return sprintf("%s%say", substr($text, 1), $text[0]);
 }
