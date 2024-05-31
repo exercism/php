0357b59a00bc94d58816b9867a69a84b7ee6f29f
mk-mxp <55182845+mk-mxp@users.noreply.github.com>
2024-05-31 17:26:17 +0200

    Sync space age (#724)

--- exercises/practice/space-age/SpaceAgeTest.php
+++ exercises/practice/space-age/SpaceAgeTest.php
@@ -1,27 +1,5 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 class SpaceAgeTest extends PHPUnit\Framework\TestCase
@@ -33,57 +11,83 @@ class SpaceAgeTest extends PHPUnit\Framework\TestCase
 
     public const DELTA = 0.01;
 
-    public function testAgeInSeconds(): void
-    {
-        $age = new SpaceAge(1000000);
-        $this->assertEquals(1000000, $age->seconds());
-    }
-
+    /**
+     * uuid 84f609af-5a91-4d68-90a3-9e32d8a5cd34
+     * @testdox Age on Earth
+     */
     public function testAgeOnEarth(): void
     {
         $age = new SpaceAge(1000000000);
         $this->assertEqualsWithDelta(31.69, $age->earth(), self::DELTA);
     }
 
+    /**
+     * uuid ca20c4e9-6054-458c-9312-79679ffab40b
+     * @testdox Age on Mercury
+     */
     public function testAgeOnMercury(): void
     {
         $age = new SpaceAge(2134835688);
         $this->assertEqualsWithDelta(280.88, $age->mercury(), self::DELTA);
     }
 
+    /**
+     * uuid 502c6529-fd1b-41d3-8fab-65e03082b024
+     * @testdox Age on Venus
+     */
     public function testAgeOnVenus(): void
     {
         $age = new SpaceAge(189839836);
         $this->assertEqualsWithDelta(9.78, $age->venus(), self::DELTA);
     }
 
+    /**
+     * uuid 9ceadf5e-a0d5-4388-9d40-2c459227ceb8
+     * @testdox Age on Mars
+     */
     public function testAgeOnMars(): void
     {
-        $age = new SpaceAge(2329871239);
-        $this->assertEqualsWithDelta(39.25, $age->mars(), self::DELTA);
+        $age = new SpaceAge(2129871239);
+        $this->assertEqualsWithDelta(35.88, $age->mars(), self::DELTA);
     }
 
+    /**
+     * uuid 42927dc3-fe5e-4f76-a5b5-f737fc19bcde
+     * @testdox Age on Jupiter
+     */
     public function testAgeOnJupiter(): void
     {
         $age = new SpaceAge(901876382);
         $this->assertEqualsWithDelta(2.41, $age->jupiter(), self::DELTA);
     }
 
+    /**
+     * uuid 8469b332-7837-4ada-b27c-00ee043ebcad
+     * @testdox Age on Saturn
+     */
     public function testAgeOnSaturn(): void
     {
-        $age = new SpaceAge(3000000000);
-        $this->assertEqualsWithDelta(3.23, $age->saturn(), self::DELTA);
+        $age = new SpaceAge(2000000000);
+        $this->assertEqualsWithDelta(2.15, $age->saturn(), self::DELTA);
     }
 
+    /**
+     * uuid 999354c1-76f8-4bb5-a672-f317b6436743
+     * @testdox Age on Uranus
+     */
     public function testAgeOnUranus(): void
     {
-        $age = new SpaceAge(3210123456);
-        $this->assertEqualsWithDelta(1.21, $age->uranus(), self::DELTA);
+        $age = new SpaceAge(1210123456);
+        $this->assertEqualsWithDelta(0.46, $age->uranus(), self::DELTA);
     }
 
+    /**
+     * uuid 80096d30-a0d4-4449-903e-a381178355d8
+     * @testdox Age on Neptune
+     */
     public function testAgeOnNeptune(): void
     {
-        $age = new SpaceAge(8210123456);
-        $this->assertEqualsWithDelta(1.58, $age->neptune(), self::DELTA);
+        $age = new SpaceAge(1821023456);
+        $this->assertEqualsWithDelta(0.35, $age->neptune(), self::DELTA);
     }
 }
