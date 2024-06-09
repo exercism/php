a2f21e9a63bb7350d415ca8097675b6367ca3043
Fejan Malek <fejan.malek@groovyweb.co>
2024-06-09 14:25:12 +0530

    change: strict types added on maintainer review and feedback

--- exercises/practice/rna-transcription/RnaTranscriptionTest.php
+++ exercises/practice/rna-transcription/RnaTranscriptionTest.php
@@ -1,8 +1,7 @@
 <?php
 
-/**
- * @uuid 123e4567-e89b-12d3-a456-426614174000
- */
+declare(strict_types=1);
+
 class RnaTranscriptionTest extends PHPUnit\Framework\TestCase
 {
     public static function setUpBeforeClass(): void
