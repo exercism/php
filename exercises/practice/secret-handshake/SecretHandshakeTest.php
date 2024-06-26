7b09bbab99b5ff324c67f1fa9fb15c1d7f3e431a
Fejan Malek <fejan.malek@groovyweb.co>
2024-06-26 08:44:47 +0530

    Change: Added only `@testdox`

--- exercises/practice/secret-handshake/SecretHandshakeTest.php
+++ exercises/practice/secret-handshake/SecretHandshakeTest.php
@@ -17,6 +17,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox Wink for 1
      * uuid: b8496fbd-6778-468c-8054-648d03c4bb23
      */
     public function testWinkForOne(): void
@@ -25,6 +26,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox Double blink for 10
      * uuid: 83ec6c58-81a9-4fd1-bfaf-0160514fc0e3
      */
     public function testDoubleBlinkForTen(): void
@@ -33,6 +35,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox close your eyes for 100
      * uuid: 0e20e466-3519-4134-8082-5639d85fef71
      */
     public function testCloseYourEyesForHundred(): void
@@ -41,6 +44,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox jump for 1000
      * uuid: b339ddbb-88b7-4b7d-9b19-4134030d9ac0
      */
     public function testJumpForThousand(): void
@@ -49,6 +53,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox combine two actions
      * uuid: 40499fb4-e60c-43d7-8b98-0de3ca44e0eb
      */
     public function testCombineTwoActions(): void
@@ -57,6 +62,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox reverse two actions
      * uuid: 9730cdd5-ef27-494b-afd3-5c91ad6c3d9d
      */
     public function testReverseTwoActions(): void
@@ -65,6 +71,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox reversing one action gives the same action
      * uuid: 0b828205-51ca-45cd-90d5-f2506013f25f
      */
     public function testReversingOneActionGivesTheSameAction(): void
@@ -73,6 +80,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox reversing no actions still gives no actions
      * uuid: 9949e2ac-6c9c-4330-b685-2089ab28b05f
      */
     public function testReversingNoActionsStillGivesNoActions(): void
@@ -81,6 +89,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox all possible actions
      * uuid: 23fdca98-676b-4848-970d-cfed7be39f81
      */
     public function testAllPossibleActions(): void
@@ -92,6 +101,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox reverse all possible actions
      * uuid: ae8fe006-d910-4d6f-be00-54b7c3799e79
      */
     public function testReverseAllPossibleActions(): void
@@ -103,6 +113,7 @@ class SecretHandshakeTest extends PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testdox do nothing for zero
      * uuid: 3d36da37-b31f-4cdb-a396-d93a2ee1c4a5
      */
     public function testDoNothingForZero(): void
