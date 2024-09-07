cf27ed902d7718ff49ecd2a43562b9e16f13b8b5
Fejan Malek <fejan.malek@groovyweb.co>
2024-09-07 12:13:31 +0530

    testdox comment added in testcases
    [no important files changed]

--- exercises/practice/rotational-cipher/RotationalCipherTest.php
+++ exercises/practice/rotational-cipher/RotationalCipherTest.php
@@ -18,6 +18,7 @@ class RotationalCipherTest extends PHPUnit\Framework\TestCase
 
     /**
      * uuid: 74e58a38-e484-43f1-9466-877a7515e10f
+     * @testdox Rotate a by 0, same output as input
      */
     public function testRotateAByZero(): void
     {
@@ -28,6 +29,7 @@ class RotationalCipherTest extends PHPUnit\Framework\TestCase
 
     /**
      * uuid: 7ee352c6-e6b0-4930-b903-d09943ecb8f5
+     * @testdox Rotate a by 1
      */
     public function testRotateAByOne(): void
     {
@@ -38,6 +40,7 @@ class RotationalCipherTest extends PHPUnit\Framework\TestCase
 
     /**
      * uuid: edf0a733-4231-4594-a5ee-46a4009ad764
+     * @testdox Rotate a by 26, same output as input
      */
     public function testRotateABy26(): void
     {
@@ -48,6 +51,7 @@ class RotationalCipherTest extends PHPUnit\Framework\TestCase
 
     /**
      * uuid: e3e82cb9-2a5b-403f-9931-e43213879300
+     * @testdox Rotate m by 13
      */
     public function testRotateMBy13(): void
     {
@@ -58,6 +62,7 @@ class RotationalCipherTest extends PHPUnit\Framework\TestCase
 
     /**
      * uuid: 19f9eb78-e2ad-4da4-8fe3-9291d47c1709
+     * @testdox Rotate n by 13 with wrap around alphabet
      */
     public function testRotateNBy13WithWrapAroundAlphabet(): void
     {
@@ -68,6 +73,7 @@ class RotationalCipherTest extends PHPUnit\Framework\TestCase
 
     /**
      * uuid: a116aef4-225b-4da9-884f-e8023ca6408a
+     * @testdox Rotate capital letters
      */
     public function testRotateCapitalLetters(): void
     {
@@ -78,6 +84,7 @@ class RotationalCipherTest extends PHPUnit\Framework\TestCase
 
     /**
      * uuid: 71b541bb-819c-4dc6-a9c3-132ef9bb737b
+     * @testdox Rotate spaces
      */
     public function testRotateSpaces(): void
     {
@@ -88,6 +95,7 @@ class RotationalCipherTest extends PHPUnit\Framework\TestCase
 
     /**
      * uuid: ef32601d-e9ef-4b29-b2b5-8971392282e6
+     * @testdox Rotate numbers
      */
     public function testRotateNumbers(): void
     {
@@ -98,6 +106,7 @@ class RotationalCipherTest extends PHPUnit\Framework\TestCase
 
     /**
      * uuid: 32dd74f6-db2b-41a6-b02c-82eb4f93e549
+     * @testdox Rotate punctuation
      */
     public function testRotatePunctuation(): void
     {
@@ -108,6 +117,7 @@ class RotationalCipherTest extends PHPUnit\Framework\TestCase
 
     /**
      * uuid: 9fb93fe6-42b0-46e6-9ec1-0bf0a062d8c9
+     * @testdox Rotate all letters
      */
     public function testRotateAllLetters(): void
     {
