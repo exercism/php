c0651099acc0ec356417a55490d8392d40043d08
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2024-10-27 20:12:24 +0100

    sync collatz conjecture

--- exercises/practice/collatz-conjecture/CollatzConjectureTest.php
+++ exercises/practice/collatz-conjecture/CollatzConjectureTest.php
@@ -1,27 +1,5 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 class CollatzConjectureTest extends PHPUnit\Framework\TestCase
@@ -31,38 +9,62 @@ class CollatzConjectureTest extends PHPUnit\Framework\TestCase
         require_once 'CollatzConjecture.php';
     }
 
+    /**
+     * uuid: 540a3d51-e7a6-47a5-92a3-4ad1838f0bfd
+     * @testdox zero steps for one
+     */
     public function testZeroStepsForOne(): void
     {
         $this->assertEquals(0, steps(1));
     }
 
+    /**
+     * uuid: 3d76a0a6-ea84-444a-821a-f7857c2c1859
+     * @testdox divide if even
+     */
     public function testDivideIfEven(): void
     {
         $this->assertEquals(4, steps(16));
     }
 
+    /**
+     * uuid: 754dea81-123c-429e-b8bc-db20b05a87b9
+     * @testdox even and odd steps
+     */
     public function testEvenAndOddSteps(): void
     {
         $this->assertEquals(9, steps(12));
     }
 
+    /**
+     * uuid: ecfd0210-6f85-44f6-8280-f65534892ff6
+     * @testdox large number of even and odd steps
+     */
     public function testLargeNumberOfEvenAndOddSteps(): void
     {
         $this->assertEquals(152, steps(1000000));
     }
 
+    /**
+     * uuid: 2187673d-77d6-4543-975e-66df6c50e2da
+     * @testdox zero is an error
+     */
     public function testZeroIsAnError(): void
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Only positive numbers are allowed');
+        $this->expectExceptionMessageMatches('/Only positive (numbers|integers) are allowed/');
 
         steps(0);
     }
 
+    /**
+     * uuid: ec11f479-56bc-47fd-a434-bcd7a31a7a2e
+     * @testdox negative value is an error
+     */
     public function testNegativeValueIsAnError(): void
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Only positive numbers are allowed');
+        $this->expectExceptionMessageMatches('/Only positive (numbers|integers) are allowed/');
 
         steps(-1);
     }
