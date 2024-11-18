9562ac8341356883f0ebe3e191d79a6c2a42b2aa
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2024-11-18 10:53:25 +0100

    Sync change (#862)

--- exercises/practice/change/ChangeTest.php
+++ exercises/practice/change/ChangeTest.php
@@ -1,27 +1,5 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 class ChangeTest extends PHPUnit\Framework\TestCase
@@ -31,26 +9,55 @@ class ChangeTest extends PHPUnit\Framework\TestCase
         require_once 'Change.php';
     }
 
+    /**
+     * uuid d0ebd0e1-9d27-4609-a654-df5c0ba1d83a
+     * @testdox change for 1 cent
+     */
+    public function testChangeForOneCent(): void
+    {
+        $this->assertEquals([1], findFewestCoins([1, 5, 10, 25], 1));
+    }
+
+    /**
+     * uuid 36887bea-7f92-4a9c-b0cc-c0e886b3ecc8
+     * @testdox single coin change
+     */
     public function testSingleCoinChange(): void
     {
         $this->assertEquals([25], findFewestCoins([1, 5, 10, 25, 100], 25));
     }
 
-    public function testChange(): void
+    /**
+     * uuid cef21ccc-0811-4e6e-af44-f011e7eab6c6
+     * @testdox multiple coin change
+     */
+    public function testMultipleCoinChange(): void
     {
         $this->assertEquals([5, 10], findFewestCoins([1, 5, 10, 25, 100], 15));
     }
 
+    /**
+     * uuid d60952bc-0c1a-4571-bf0c-41be72690cb3
+     * @testdox change with Lilliputian Coins
+     */
     public function testChangeWithLilliputianCoins(): void
     {
         $this->assertEquals([4, 4, 15], findFewestCoins([1, 4, 15, 20, 50], 23));
     }
 
+    /**
+     * uuid 408390b9-fafa-4bb9-b608-ffe6036edb6c
+     * @testdox change with Lower Elbonia Coins
+     */
     public function testChangeWithLowerElboniaCoins(): void
     {
         $this->assertEquals([21, 21, 21], findFewestCoins([1, 5, 10, 21, 25], 63));
     }
 
+    /**
+     * uuid 7421a4cb-1c48-4bf9-99c7-7f049689132f
+     * @testdox large target values
+     */
     public function testWithLargeTargetValue(): void
     {
         $this->assertEquals(
@@ -59,6 +66,10 @@ class ChangeTest extends PHPUnit\Framework\TestCase
         );
     }
 
+    /**
+     * uuid f79d2e9b-0ae3-4d6a-bb58-dc978b0dba28
+     * @testdox possible change without unit coins available
+     */
     public function testPossibleChangeWithoutUnitCoinsAvailable(): void
     {
         $this->assertEquals(
@@ -67,6 +78,10 @@ class ChangeTest extends PHPUnit\Framework\TestCase
         );
     }
 
+    /**
+     * uuid 9a166411-d35d-4f7f-a007-6724ac266178
+     * @testdox another possible change without unit coins available
+     */
     public function testAnotherPossibleChangeWithoutUnitCoinsAvailable(): void
     {
         $this->assertEquals(
@@ -75,11 +90,28 @@ class ChangeTest extends PHPUnit\Framework\TestCase
         );
     }
 
+    /**
+     * uuid ce0f80d5-51c3-469d-818c-3e69dbd25f75
+     * @testdox a greedy approach is not optimal
+     */
+    public function testAGreedyApproachIsNotOptimal(): void
+    {
+        $this->assertEquals([10, 10], findFewestCoins([1, 10, 11], 20));
+    }
+
+    /**
+     * uuid bbbcc154-e9e9-4209-a4db-dd6d81ec26bb
+     * @testdox no coins make 0 change
+     */
     public function testNoCoinsForZero(): void
     {
         $this->assertEquals([], findFewestCoins([1, 5, 10, 21, 25], 0));
     }
 
+    /**
+     * uuid c8b81d5a-49bd-4b61-af73-8ee5383a2ce1
+     * @testdox error testing for change smaller than the smallest of coins
+     */
     public function testForChangeSmallerThanAvailableCoins(): void
     {
         $this->expectException(InvalidArgumentException::class);
@@ -88,6 +120,10 @@ class ChangeTest extends PHPUnit\Framework\TestCase
         findFewestCoins([5, 10], 3);
     }
 
+    /**
+     * uuid 3c43e3e4-63f9-46ac-9476-a67516e98f68
+     * @testdox error if no combination can add up to target
+     */
     public function testErrorIfNoCombinationCanAddUpToTarget(): void
     {
         $this->expectException(InvalidArgumentException::class);
@@ -96,6 +132,10 @@ class ChangeTest extends PHPUnit\Framework\TestCase
         findFewestCoins([5, 10], 94);
     }
 
+    /**
+     * uuid 8fe1f076-9b2d-4f44-89fe-8a6ccd63c8f3
+     * @testdox cannot find negative change values
+     */
     public function testChangeValueLessThanZero(): void
     {
         $this->expectException(InvalidArgumentException::class);
