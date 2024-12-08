0af034f0932d40869af914df7131b3bfd6bd8eff
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2024-12-08 10:14:33 +0100

    Sync book-store (#850)

--- exercises/practice/book-store/.meta/example.php
+++ exercises/practice/book-store/.meta/example.php
@@ -1,62 +1,42 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
-declare(strict_types=1);
-
-function total($items)
+function total(array $items): int
 {
-    return calculate($items, 0);
-}
+    $groups = array_fill(1, 5, 0); // Groups of books
+    $sets = array_fill(1, 5, 0);   // Sets by size
 
-function calculate($items, $total)
-{
-    if (empty($items)) {
-        return $total;
+    // Group the basket
+    foreach ($items as $bookId) {
+        $groups[$bookId]++;
     }
 
-    $group = array_unique($items);
-    $totalMin = INF;
-
-    for ($i = count($group); $i > 0; $i--) {
-        $itemsToRemove = array_flip(array_slice($group, 0, $i));
-        $itemsRemaining = array_filter($items, function ($x) use (&$itemsToRemove) {
-            if (array_key_exists($x, $itemsToRemove)) {
-                unset($itemsToRemove[$x]);
-                return false;
+    // Arrange groups into counts by set size
+    while (array_sum($groups) > 0) {
+        $setSize = 0;
+        foreach ($groups as $key => $count) {
+            if ($count > 0) {
+                $setSize++;
+                $groups[$key]--;
             }
-            return true;
-        });
-        $totalCurrent = calculate($itemsRemaining, $total + totalForGroup($i));
-        $totalMin = min([$totalMin, $totalCurrent]);
+        }
+        if ($setSize > 0) {
+            $sets[$setSize]++;
+        }
     }
 
-    return $totalMin;
-}
+    // Replace each 3set+5set with two 4sets
+    while ($sets[3] > 0 && $sets[5] > 0) {
+        $sets[3]--;
+        $sets[5]--;
+        $sets[4] += 2;
+    }
 
-function totalForGroup($count)
-{
-    $discount = [0, 0, 0.05, 0.1, 0.2, 0.25];
-    $price = 8;
-    return $price * $count * (1 - $discount[$count]);
+    // Calculate the total cost
+    $cost = 800 * $sets[1] +
+        (800 * 2 * 0.95) * $sets[2] +
+        (800 * 3 * 0.9) * $sets[3] +
+        (800 * 4 * 0.8) * $sets[4] +
+        (800 * 5 * 0.75) * $sets[5];
+
+    return (int)$cost;
 }
--- exercises/practice/book-store/BookStoreTest.php
+++ exercises/practice/book-store/BookStoreTest.php
@@ -1,27 +1,5 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 /**
@@ -41,71 +19,92 @@ class BookStoreTest extends PHPUnit\Framework\TestCase
     /**
      * A basket containing only a single book.
      * Target grouping: [[1]]
+     *
+     * uuid 17146bd5-2e80-4557-ab4c-05632b6b0d01
+     * @testdox Only a single book
      */
     public function testSingleBook(): void
     {
         $basket = [1];
-        $this->assertEquals(8.0, total($basket));
+        $this->assertEquals(800, total($basket));
     }
 
     /**
      * A basket containing only two of the same book.
      * Target grouping: [[2], [2]]
+     *
+     * uuid cc2de9ac-ff2a-4efd-b7c7-bfe0f43271ce
+     * @testdox Two of the same book
      */
     public function testTwoSame(): void
     {
         $basket = [2, 2];
-        $this->assertEquals(16.0, total($basket));
+        $this->assertEquals(1600, total($basket));
     }
 
     /**
      * No charge to carry around an empty basket.
      * Target grouping: []
+     *
+     * uuid 5a86eac0-45d2-46aa-bbf0-266b94393a1a
+     * @testdox Empty basket
      */
     public function testEmpty(): void
     {
         $basket = [];
-        $this->assertEquals(0.0, total($basket));
+        $this->assertEquals(0, total($basket));
     }
 
     /**
      * A basket containing only two different books.
      * Target grouping: [[1, 2]]
+     *
+     * uuid 158bd19a-3db4-4468-ae85-e0638a688990
+     * @testdox Two different books
      */
     public function testTwoDifferent(): void
     {
         $basket = [1, 2];
-        $this->assertEquals(15.2, total($basket));
+        $this->assertEquals(1520, total($basket));
     }
 
     /**
      * A basket of three different books.
      * Target grouping: [[1, 2, 3]]
+     *
+     * uuid f3833f6b-9332-4a1f-ad98-6c3f8e30e163
+     * @testdox Three different books
      */
     public function testThreeDifferent(): void
     {
         $basket = [1, 2, 3];
-        $this->assertEquals(21.60, total($basket));
+        $this->assertEquals(2160, total($basket));
     }
 
     /**
      * A basket of four different books.
      * Target grouping: [[1, 2, 3, 4]]
+     *
+     * uuid 1951a1db-2fb6-4cd1-a69a-f691b6dd30a2
+     * @testdox Four different books
      */
     public function testFourDifferent(): void
     {
         $basket = [1, 2, 3, 4];
-        $this->assertEquals(25.60, total($basket));
+        $this->assertEquals(2560, total($basket));
     }
 
     /**
      * A basket of five different books.
      * Target grouping: [[1, 2, 3, 4, 5]]
+     *
+     * uuid d70f6682-3019-4c3f-aede-83c6a8c647a3
+     * @testdox Five different books
      */
     public function testFiveDifferent(): void
     {
         $basket = [1, 2, 3, 4, 5];
-        $this->assertEquals(30.00, total($basket));
+        $this->assertEquals(3000, total($basket));
     }
 
     /**
@@ -116,11 +115,38 @@ class BookStoreTest extends PHPUnit\Framework\TestCase
      * is not intuitive, but does grant the largest
      * discount.
      * Target grouping: [[1, 2, 3, 4], [1, 2, 3, 5]]
+     *
+     * uuid 78cacb57-911a-45f1-be52-2a5bd428c634
+     * @testdox Two groups of four is cheaper than group of five plus group of three
      */
     public function testEight(): void
     {
         $basket = [1, 1, 2, 2, 3, 3, 4, 5];
-        $this->assertEquals(51.20, total($basket));
+        $this->assertEquals(5120, total($basket));
+    }
+
+    /**
+     * Target Grouping: [[1, 2, 4, 5], [1, 3, 4, 5]]
+     *
+     * uuid f808b5a4-e01f-4c0d-881f-f7b90d9739da
+     * @testdox Two groups of four is cheaper than groups of five and three
+     */
+    public function testTwoGroupsOfFourIsCheaperThanGroupsOfFiveAndThree(): void
+    {
+        $basket = [1, 1, 2, 3, 4, 4, 5, 5];
+        $this->assertEquals(5120, total($basket));
+    }
+
+    /**
+     * Target Grouping: [[1, 2, 3, 4], [1, 2]]
+     *
+     * uuid fe96401c-5268-4be2-9d9e-19b76478007c
+     * @testdox Group of four plus group of two is cheaper than two groups of three
+     */
+    public function testGroupOfFourPlusGroupOfTwoIsCheaperThanTwoGroupsOfThree(): void
+    {
+        $basket = [1, 1, 2, 2, 3, 4];
+        $this->assertEquals(4080, total($basket));
     }
 
     /**
@@ -128,22 +154,28 @@ class BookStoreTest extends PHPUnit\Framework\TestCase
      * pair each of the first four books plus one of
      * the last book.
      * Target grouping: [[1, 2, 3, 4, 5], [1, 2, 3, 4]],
+     *
+     * uuid 68ea9b78-10ad-420e-a766-836a501d3633
+     * @testdox Two each of first four books and one copy each of rest
      */
     public function testFourPairsPlusOne(): void
     {
         $basket = [1, 1, 2, 2, 3, 3, 4, 4, 5];
-        $this->assertEquals(55.60, total($basket));
+        $this->assertEquals(5560, total($basket));
     }
 
     /**
      * A basket containing ten books consisting of two
      * copies of each book in the series.
      * Target grouping: [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+     *
+     * uuid c0a779d5-a40c-47ae-9828-a340e936b866
+     * @testdox Two copies of each boo
      */
     public function testFivePairs(): void
     {
         $basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
-        $this->assertEquals(60.00, total($basket));
+        $this->assertEquals(6000, total($basket));
     }
 
     /**
@@ -151,11 +183,14 @@ class BookStoreTest extends PHPUnit\Framework\TestCase
      * of three copies of the first book plus two each
      * of the remaining four books in the series.
      * Target grouping: [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1]]
+     *
+     * uuid 18fd86fe-08f1-4b68-969b-392b8af20513
+     * @testdox Three copies of first book and two each of remaining
      */
     public function testFivePairsPlusOne(): void
     {
         $basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1];
-        $this->assertEquals(68.00, total($basket));
+        $this->assertEquals(6800, total($basket));
     }
 
     /**
@@ -163,10 +198,61 @@ class BookStoreTest extends PHPUnit\Framework\TestCase
      * three copies of the first two books, plus two
      * each of the remaining three books in the series.
      * Target grouping: [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2]]
+     *
+     * uuid 0b19a24d-e4cf-4ec8-9db2-8899a41af0da
+     * @testdox Three each of first two books and two each of remaining books
      */
     public function testTwelve(): void
     {
         $basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 2];
-        $this->assertEquals(75.20, total($basket));
+        $this->assertEquals(7520, total($basket));
+    }
+
+    /**
+     * Target Grouping: [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 3, 4], [1, 2, 3, 5]]
+     *
+     * uuid bb376344-4fb2-49ab-ab85-e38d8354a58d
+     * @testdox Four groups of four are cheaper than two groups each of five and three
+     */
+    public function testFourGroupsOfFourAreCheaperThanTwoGroupsEachOfFiveAndThree(): void
+    {
+        $basket = [1, 1, 2, 2, 3, 3, 4, 5, 1, 1, 2, 2, 3, 3, 4, 5];
+        $this->assertEquals(10240, total($basket));
+    }
+
+    /**
+     * Target Grouping: [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 3], [1, 2, 3]]
+     *
+     * uuid 5260ddde-2703-4915-b45a-e54dbbac4303
+     * @testdox Check that groups of 4 are created properly even when there are more groups of 3 than groups of 5
+     */
+    public function testCheckThatGroupsOfFourAreCreatedProperlyEvenWhenThereAreMoreGroupsOfThreeThanGroupsOfFive(): void
+    {
+        $basket = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 5, 5];
+        $this->assertEquals(14560, total($basket));
+    }
+
+    /**
+     * Target Grouping: [[1], [1, 2, 3, 4]]
+     *
+     * uuid b0478278-c551-4747-b0fc-7e0be3158b1f
+     * @testdox One group of one and four is cheaper than one group of two and three
+     */
+    public function testOneGroupOfOneAndFourIsCheaperThanOneGroupOfTwoAndThree(): void
+    {
+        $basket = [1, 1, 2, 3, 4];
+        $this->assertEquals(3360, total($basket));
+    }
+
+    /**
+     * Target Grouping: [[5], [5, 4], [5, 4, 3, 2], [5, 4, 3, 2], [5, 4, 3, 1]]
+     *
+     * uuid cf868453-6484-4ae1-9dfc-f8ee85bbde01
+     * @testdox One group of one and two plus three groups of four is cheaper than one group of each size
+     */
+    public function testOneGroupOfOneAndTwoPlusThreeGroupsOfFourIsCheaperThanOneGroupOfEachSize(): void
+    {
+        $basket = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5];
+        $this->assertEquals(10000, total($basket));
     }
 }
