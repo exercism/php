339eb3e407a7520847935f14d5b90a39f2f65044
Michael Kramer <m.kramer@mxp.de>
2025-02-09 16:39:03 +0100

    Provide a script to sync known-good exercises
    This script uses a manually prepared list of practice exercises that are
    known to be "good for automatic updating".
    
    "Good for automatic updating" means, these exercises were manually
    updated to the latest version of the problem specifications, have all
    tests implemented according to `tests.toml`, and so may be automatically
    updated by a scheduled CI job (not including the tests).

---
+++ bin/auto-sync.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+scriptDirectory="$( dirname "$0" )"
+if [[ "$#" != 0 ]]; then
+    forwardedParameters=( "$@" )
+else
+    forwardedParameters=( -o -u -y --docs --filepaths --metadata )
+fi
+
+exitWithFailure() {
+    echo "$1"
+    exit 1
+}
+
+isPracticeExercise() {
+    [ -d "exercises/practice/$1" ]
+}
+
+reportUnknownExercise() {
+    echo "Unknown practice exercise: $1"
+}
+
+isNoPracticeExercise() {
+    isPracticeExercise "$1" || {
+        reportUnknownExercise "$1"
+        return 0
+    }
+    return 1
+}
+
+reportRepeatedExercise() {
+    echo "Repeated exercise slug: $1"
+}
+
+syncPracticeExercise() {
+    local exerciseSlug="$1"
+
+    bin/configlet sync -v q -e "$exerciseSlug" "${forwardedParameters[@]}"
+}
+
+main() {
+    local -A uniqueSlugs=()
+    local -a exerciseSlugs
+    local exerciseSlug
+
+    # Refresh 'problem-spec' cache once
+    bin/configlet info || exitWithFailure "configlet not ready to run offline"
+
+    mapfile -t exerciseSlugs < "$scriptDirectory/auto-sync.txt"
+
+    for exerciseSlug in "${exerciseSlugs[@]}"; do
+        if [[ "${uniqueSlugs[$exerciseSlug]}" == "set" ]]; then
+            reportRepeatedExercise "$exerciseSlug"
+        else
+            uniqueSlugs[$exerciseSlug]="set"
+            isNoPracticeExercise "${exerciseSlug}" && continue
+            syncPracticeExercise "${exerciseSlug}"
+        fi
+    done
+}
+
+main
