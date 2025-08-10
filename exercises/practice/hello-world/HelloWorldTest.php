094c1b18c3505f9f8f38384e80dcf02d786bb5fb
Michael Kramer <m.kramer@mxp.de>
2025-08-10 14:01:25 +0200

    Add and sync test meta data to tests

--- exercises/practice/hello-world/HelloWorldTest.php
+++ exercises/practice/hello-world/HelloWorldTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\TestDox;
 
 class HelloWorldTest extends TestCase
 {
@@ -11,6 +12,10 @@ class HelloWorldTest extends TestCase
         require_once 'HelloWorld.php';
     }
 
+    /**
+     * uuid: af9ffe10-dc13-42d8-a742-e7bdafac449d
+     */
+    #[TestDox('Say Hi!')]
     public function testHelloWorld(): void
     {
         $this->assertEquals('Hello, World!', helloWorld());
