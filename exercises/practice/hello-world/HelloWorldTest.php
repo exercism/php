1480be0053548e2fb26c9e08cb77341eeda124fc
mk-mxp <55182845+mk-mxp@users.noreply.github.com>
2025-08-11 09:17:29 +0200

    Sync hello-world (#909)

--- exercises/practice/hello-world/HelloWorldTest.php
+++ exercises/practice/hello-world/HelloWorldTest.php
@@ -1,30 +1,9 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\TestDox;
 
 class HelloWorldTest extends TestCase
 {
@@ -33,6 +12,10 @@ class HelloWorldTest extends TestCase
         require_once 'HelloWorld.php';
     }
 
+    /**
+     * uuid: af9ffe10-dc13-42d8-a742-e7bdafac449d
+     */
+    #[TestDox('Say Hi!')]
     public function testHelloWorld(): void
     {
         $this->assertEquals('Hello, World!', helloWorld());
