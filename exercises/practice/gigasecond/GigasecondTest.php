675a2daa847cbdbff4cf468af105fd7291ac8de3
mk-mxp <55182845+mk-mxp@users.noreply.github.com>
2025-08-23 13:36:20 +0200

    Sync gigasecond (#916)
    [no important files changed]

--- exercises/practice/gigasecond/GigasecondTest.php
+++ exercises/practice/gigasecond/GigasecondTest.php
@@ -1,32 +1,13 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\TestDox;
 
+/**
+ * ATTENTION: We use function `from()` for backwards compatibility, not `add()`
+ */
 class GigasecondTest extends TestCase
 {
     public static function setUpBeforeClass(): void
@@ -34,54 +15,83 @@ class GigasecondTest extends TestCase
         require_once 'Gigasecond.php';
     }
 
-    public function dateSetup($date): DateTimeImmutable
+    /** uuid: 92fbe71c-ea52-4fac-bd77-be38023cacf7 */
+    #[TestDox('Date only specification of time')]
+    public function testDateOnlySpecificationOfTime(): void
     {
         $UTC = new DateTimeZone('UTC');
-        return new DateTimeImmutable($date, $UTC);
+        $input = new DateTimeImmutable('2011-04-25', $UTC);
+
+        $actual = from($input);
+
+        $this->assertInstanceOf(DateTimeImmutable::class, $actual);
+        $this->assertSame(
+            '2043-01-01 01:46:40',
+            $actual->format('Y-m-d H:i:s'),
+        );
     }
 
-    public static function inputAndExpectedDates(): array
+    /** uuid: 6d86dd16-6f7a-47be-9e58-bb9fb2ae1433 */
+    #[TestDox('Second test for date only specification of time')]
+    public function testSecondTestForDateOnlySpecificationOfTime(): void
     {
-        return [
-            ['2011-04-25', '2043-01-01 01:46:40'],
-            ['1977-06-13', '2009-02-19 01:46:40'],
-            ['1959-07-19', '1991-03-27 01:46:40'],
-            ['2015-01-24 22:00:00', '2046-10-02 23:46:40'],
-            ['2015-01-24 23:59:59', '2046-10-03 01:46:39'],
-        ];
+        $UTC = new DateTimeZone('UTC');
+        $input = new DateTimeImmutable('1977-06-13', $UTC);
+
+        $actual = from($input);
+
+        $this->assertInstanceOf(DateTimeImmutable::class, $actual);
+        $this->assertSame(
+            '2009-02-19 01:46:40',
+            $actual->format('Y-m-d H:i:s'),
+        );
     }
 
-    public static function inputDates(): array
+    /** uuid: 77eb8502-2bca-4d92-89d9-7b39ace28dd5 */
+    #[TestDox('Third test for date only specification of time')]
+    public function testThirdTestForDateOnlySpecificationOfTime(): void
     {
-        return [
-            ['2011-04-25'],
-            ['1977-06-13'],
-            ['1959-07-19'],
-            ['2015-01-24 22:00:00'],
-            ['2015-01-24 23:59:59'],
-        ];
+        $UTC = new DateTimeZone('UTC');
+        $input = new DateTimeImmutable('1959-07-19', $UTC);
+
+        $actual = from($input);
+
+        $this->assertInstanceOf(DateTimeImmutable::class, $actual);
+        $this->assertSame(
+            '1991-03-27 01:46:40',
+            $actual->format('Y-m-d H:i:s'),
+        );
     }
 
-    /**
-     * @param string $inputDate
-     * @param string $expected
-     */
-    #[DataProvider('inputAndExpectedDates')]
-    public function testFrom(string $inputDate, string $expected): void
+    /** uuid: c9d89a7d-06f8-4e28-a305-64f1b2abc693 */
+    #[TestDox('Full time specified')]
+    public function testFullTimeSpecified(): void
     {
-        $date = $this->dateSetup($inputDate);
-        $gs = from($date);
+        $UTC = new DateTimeZone('UTC');
+        $input = new DateTimeImmutable('2015-01-24 22:00:00', $UTC);
 
-        $this->assertSame($expected, $gs->format('Y-m-d H:i:s'));
+        $actual = from($input);
+
+        $this->assertInstanceOf(DateTimeImmutable::class, $actual);
+        $this->assertSame(
+            '2046-10-02 23:46:40',
+            $actual->format('Y-m-d H:i:s'),
+        );
     }
 
-    /**
-     * @param string $inputDate
-     */
-    #[DataProvider('inputDates')]
-    public function testFromReturnType(string $inputDate): void
+    /** uuid: 09d4e30e-728a-4b52-9005-be44a58d9eba */
+    #[TestDox('Full time with day roll-over')]
+    public function testFullTimeWithDayRollOver(): void
     {
-        $date = $this->dateSetup($inputDate);
-        $this->assertInstanceOf(DateTimeImmutable::class, from($date));
+        $UTC = new DateTimeZone('UTC');
+        $input = new DateTimeImmutable('2015-01-24 23:59:59', $UTC);
+
+        $actual = from($input);
+
+        $this->assertInstanceOf(DateTimeImmutable::class, $actual);
+        $this->assertSame(
+            '2046-10-03 01:46:39',
+            $actual->format('Y-m-d H:i:s'),
+        );
     }
 }
