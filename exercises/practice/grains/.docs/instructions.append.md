0d21ef27d81288f2d94a73adf692eec48de4a496
Obinna Emmanuel Obi-Akwari <emmanuel0058@gmail.com>
2025-10-07 00:18:15 +0100

    move comments to instruction.append.md

---
+++ exercises/practice/grains/.docs/instructions.append.md
@@ -0,0 +1,12 @@
+
+    /**
+     * Consider King hates floats and demands solution with
+     * precise integer-only calculations. Don't involve any floats.
+     * Don't use gmp or any other similar libraries.
+     * Try to make the solution for virtually any board size.
+     *
+     * PHP integers greater than 2^31 (32-bit systems)
+     * or 2^63 (64-bit) are casted to floats.
+     * In some cases it may lead to problems
+     * https://php.net/manual/ru/language.types.float.php#warn.float-precision
+     */
--- exercises/practice/grains/GrainsTest.php
+++ exercises/practice/grains/GrainsTest.php
@@ -13,18 +13,6 @@ class GrainsTest extends TestCase
     }
 
     /**
-     * PHP integers greater than 2^31 (32-bit systems)
-     * or 2^63 (64-bit) are casted to floats.
-     * In some cases it may lead to problems
-     * https://php.net/manual/ru/language.types.float.php#warn.float-precision
-     *
-     * Consider King hates floats and demands solution with
-     * precise integer-only calculations. Don't involve any floats.
-     * Don't use gmp or any other similar libraries.
-     * Try to make the solution for virtually any board size.
-     */
-
-    /**
      * uuid: 9fbde8de-36b2-49de-baf2-cd42d6f28405
      */
     #[TestDox('grains on square 1')]
