1970ec16cfe30c37bb5f1728260eb3fa4513337e
narkunan <narkunan9585@gmail.com>
2026-04-26 18:21:40 +0530

    Updated Test case for matrix

--- exercises/practice/matrix/MatrixTest.php
+++ exercises/practice/matrix/MatrixTest.php
@@ -1,27 +1,5 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 use PHPUnit\Framework\Attributes\TestDox;
@@ -101,8 +79,8 @@ class MatrixTest extends TestCase
     #[TestDox('extract column where numbers have different widths')]
     public function testExtractColumnWhenNumbersHaveDifferentWidths(): void
     {
-        $matrix = new Matrix("89 1903 3\n18 3 1\n9 41 800");
+        $matrix = new Matrix("89 1903 3\n18 3 1\n9 4 800");
 
-        $this->assertEquals([1903, 3, 41], $matrix->getColumn(2));
+        $this->assertEquals([1903, 3, 4], $matrix->getColumn(2));
     }
 }
