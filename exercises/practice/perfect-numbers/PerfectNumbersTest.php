618107d6679481fd5a7ea7847e3c8e3765a7d14f
Obinna Obi-Akwari <111562983+A-O-Emmanuel@users.noreply.github.com>
2026-05-19 16:04:51 +0100

    Sync perfect-numbers (#963)

--- exercises/practice/perfect-numbers/PerfectNumbersTest.php
+++ exercises/practice/perfect-numbers/PerfectNumbersTest.php
@@ -1,30 +1,9 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\TestDox;
 
 class PerfectNumbersTest extends TestCase
 {
@@ -33,69 +12,132 @@ class PerfectNumbersTest extends TestCase
         require_once 'PerfectNumbers.php';
     }
 
-    public function testSmallPerfectNumberIsClassifiedCorrectly(): void
+    /**
+     * uuid: 163e8e86-7bfd-4ee2-bd68-d083dc3381a3
+     */
+    #[TestDox('Perfect numbers -> Smallest perfect number is classified correctly')]
+    public function testPerfectNumbersSmallPerfectNumberIsClassifiedCorrectly(): void
     {
         $this->assertEquals("perfect", getClassification(6));
     }
 
-    public function testMediumPerfectNumberIsClassifiedCorrectly(): void
+     /**
+     * uuid: 169a7854-0431-4ae0-9815-c3b6d967436d
+     */
+    #[TestDox('Perfect numbers -> Medium perfect number is classified correctly')]
+    public function testPerfectNumbersMediumPerfectNumberIsClassifiedCorrectly(): void
     {
         $this->assertEquals("perfect", getClassification(28));
     }
 
-    public function testLargePerfectNumberIsClassifiedCorrectly(): void
+      /**
+     * uuid: ee3627c4-7b36-4245-ba7c-8727d585f402
+     */
+    #[TestDox('Perfect numbers -> Large perfect number is classified correctly')]
+    public function testPerfectNumbersLargePerfectNumberIsClassifiedCorrectly(): void
     {
         $this->assertEquals("perfect", getClassification(33550336));
     }
 
-    public function testSmallAbundantNumberIsClassifiedCorrectly(): void
+
+    /**
+     * uuid: 80ef7cf8-9ea8-49b9-8b2d-d9cb3db3ed7e
+     */
+    #[TestDox('Abundant numbers -> Smallest abundant number is classified correctly')]
+    public function testAbundantNumbersSmallAbundantNumberIsClassifiedCorrectly(): void
     {
         $this->assertEquals("abundant", getClassification(12));
     }
 
-    public function testMediumAbundantNumberIsClassifiedCorrectly(): void
+
+    /**
+     * uuid: 3e300e0d-1a12-4f11-8c48-d1027165ab60
+     */
+    #[TestDox('Abundant numbers -> Medium abundant number is classified correctly')]
+    public function testAbundantNumbersMediumAbundantNumberIsClassifiedCorrectly(): void
     {
         $this->assertEquals("abundant", getClassification(30));
     }
 
-    public function testLargeAbundantNumberIsClassifiedCorrectly(): void
+     /**
+     * uuid: ec7792e6-8786-449c-b005-ce6dd89a772b
+     */
+    #[TestDox('Abundant numbers -> Large abundant number is classified correctly')]
+    public function testAbundantNumbersLargeAbundantNumberIsClassifiedCorrectly(): void
     {
         $this->assertEquals("abundant", getClassification(33550335));
     }
 
-    public function testSmallestPrimeDeficientNumberIsClassifiedCorrectly(): void
+    /**
+     * uuid: 05f15b93-849c-45e9-9c7d-1ea131ef7d10
+     */
+    #[TestDox('Abundant numbers -> Perfect square abundant number is classified correctly')]
+    public function testAbundantNumbersPerfectSquareAbundantNumberIsClassifiedCorrectly(): void
+    {
+        $this->assertEquals("abundant", getClassification(196));
+    }
+
+    /**
+     * uuid: e610fdc7-2b6e-43c3-a51c-b70fb37413ba
+     */
+    #[TestDox('Deficient numbers -> Smallest prime deficient number is classified correctly')]
+    public function testDeficientNumbersSmallestPrimeDeficientNumberIsClassifiedCorrectly(): void
     {
         $this->assertEquals("deficient", getClassification(2));
     }
 
-    public function testSmallestNonPrimeDeficientNumberIsClassifiedCorrectly(): void
+     /**
+     * uuid: 0beb7f66-753a-443f-8075-ad7fbd9018f3
+     */
+    #[TestDox('Deficient numbers -> Smallest non-prime deficient number is classified correctly')]
+    public function testDeficientNumbersSmallestNonPrimeDeficientNumberIsClassifiedCorrectly(): void
     {
         $this->assertEquals("deficient", getClassification(4));
     }
 
-    public function testMediumDeficientNumberIsClassifiedCorrectly(): void
+     /**
+     * uuid: 1c802e45-b4c6-4962-93d7-1cad245821ef
+     */
+    #[TestDox('Deficient numbers -> Medium deficient number is classified correctly')]
+    public function testDeficientNumbersMediumDeficientNumberIsClassifiedCorrectly(): void
     {
         $this->assertEquals("deficient", getClassification(32));
     }
 
-    public function testLargeDeficientNumberIsClassifiedCorrectly(): void
+    /**
+     * uuid: 47dd569f-9e5a-4a11-9a47-a4e91c8c28aa
+     */
+    #[TestDox('Deficient numbers -> Large deficient number is classified correctly')]
+    public function testDeficientNumbersLargeDeficientNumberIsClassifiedCorrectly(): void
     {
         $this->assertEquals("deficient", getClassification(33550337));
     }
 
-    public function testThatOneIsCorrectlyClassifiedAsDeficient(): void
+    /**
+     * uuid: a696dec8-6147-4d68-afad-d38de5476a56
+     */
+    #[TestDox('Deficient numbers -> Edge case (no factors other than itself) is classified correctly')]
+    public function testDeficientNumbersThatOneIsCorrectlyClassifiedAsDeficient(): void
     {
         $this->assertEquals("deficient", getClassification(1));
     }
 
-    public function testThatNonNegativeIntegerIsRejected(): void
+    /**
+     * uuid: 72445cee-660c-4d75-8506-6c40089dc302
+     */
+    #[TestDox('Invalid inputs -> Zero is rejected (as it is not a positive integer)')]
+    public function testInvalidInputsThatNonNegativeIntegerIsRejected(): void
     {
         $this->expectException(InvalidArgumentException::class);
 
         getClassification(0);
     }
 
-    public function testThatNegativeIntegerIsRejected(): void
+    /**
+     * uuid: 2d72ce2c-6802-49ac-8ece-c790ba3dae13
+     */
+    #[TestDox('Invalid inputs -> Negative integer is rejected (as it is not a positive integer)')]
+    public function testInvalidInputsThatNegativeIntegerIsRejected(): void
     {
         $this->expectException(InvalidArgumentException::class);
 
