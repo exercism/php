2131e25a7c36e26eef3a2eb63d9b3aba6c695068
resu-xuniL <linux@lavache.com>
2026-05-26 16:12:42 +0200

    sync tests file uuid and description with test file

--- exercises/practice/two-fer/TwoFerTest.php
+++ exercises/practice/two-fer/TwoFerTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\TestDox;
 
 class TwoFerTest extends TestCase
 {
@@ -11,16 +12,28 @@ class TwoFerTest extends TestCase
         require_once 'TwoFer.php';
     }
 
+    /**
+     * uuid 1cf3e15a-a3d7-4a87-aeb3-ba1b43bc8dce
+     */
+    #[TestDox('no name given')]
     public function testNoNameGiven(): void
     {
         $this->assertEquals('One for you, one for me.', twoFer());
     }
 
+    /**
+     * uuid b4c6dbb8-b4fb-42c2-bafd-10785abe7709
+     */
+    #[TestDox('a name given')]
     public function testANameGiven(): void
     {
         $this->assertEquals('One for Alice, one for me.', twoFer('Alice'));
     }
 
+    /**
+     * uuid 3549048d-1a6e-4653-9a79-b0bda163e8d5
+     */
+    #[TestDox('another name given')]
     public function testAnotherNameGiven(): void
     {
         $this->assertEquals('One for Bob, one for me.', twoFer('Bob'));
