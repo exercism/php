e1168555adc581d1e019143b3946b60cff7258fd
resu-xuniL <linux@lavache.com>
2026-07-08 10:44:30 +0200

    Harmonise

--- exercises/practice/markdown/MarkdownTest.php
+++ exercises/practice/markdown/MarkdownTest.php
@@ -24,8 +24,8 @@
 
 declare(strict_types=1);
 
-use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\TestDox;
 
 class MarkdownTest extends TestCase
 {
