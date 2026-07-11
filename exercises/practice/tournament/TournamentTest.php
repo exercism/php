79d4119e0c4d401220c8957feedad3eb32ff0056
Michael Kramer <m.kramer@mxp.de>
2026-07-11 16:19:00 +0200

    Fix double use

--- exercises/practice/tournament/TournamentTest.php
+++ exercises/practice/tournament/TournamentTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\Attributes\TestDox;
 
 class TournamentTest extends TestCase
 {
