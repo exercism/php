6c458abc99e718d776e62fa128eb20ca7b35e30d
resu-xuniL <linux@lavache.com>
2026-07-13 22:27:52 +0200

    Harmonise `use` statement

--- exercises/practice/circular-buffer/CircularBufferTest.php
+++ exercises/practice/circular-buffer/CircularBufferTest.php
@@ -3,10 +3,9 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
 
 require_once 'CircularBuffer.php';
-
-use PHPUnit\Framework\TestCase;
 
 class CircularBufferTest extends TestCase
 {
