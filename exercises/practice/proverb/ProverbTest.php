4472550bfcd8c2a55673b55b2a1b39f5215085b4
resu-xuniL <linux@lavache.com>
2026-07-14 16:10:22 +0200

    Inlined object instantiation in `ProverbTest`

--- exercises/practice/proverb/ProverbTest.php
+++ exercises/practice/proverb/ProverbTest.php
@@ -7,65 +7,63 @@ use PHPUnit\Framework\TestCase;
 
 class ProverbTest extends TestCase
 {
-    private Proverb $proverb;
-
     public static function setUpBeforeClass(): void
     {
         require_once 'Proverb.php';
-    }
-
-    public function setUp(): void
-    {
-        $this->proverb = new Proverb();
     }
 
     /** @uuid e974b73e-7851-484f-8d6d-92e07fe742fc */
     #[TestDox('Zero pieces')]
     public function testZeroPieces(): void
     {
+        $proverb = new Proverb();
         $pieces   = [];
         $expected = [];
-        $this->assertEquals($expected, $this->proverb->recite($pieces));
+        $this->assertEquals($expected, $proverb->recite($pieces));
     }
 
     /** @uuid 2fcd5f5e-8b82-4e74-b51d-df28a5e0faa4 */
     #[TestDox('One piece')]
     public function testOnePiece(): void
     {
+        $proverb = new Proverb();
         $pieces   = ['nail'];
         $expected = ['And all for the want of a nail.'];
-        $this->assertEquals($expected, $this->proverb->recite($pieces));
+        $this->assertEquals($expected, $proverb->recite($pieces));
     }
 
     /** @uuid d9d0a8a1-d933-46e2-aa94-eecf679f4b0e */
     #[TestDox('Two pieces')]
     public function testTwoPieces(): void
     {
+        $proverb = new Proverb();
         $pieces   = ['nail', 'shoe'];
         $expected = [
             'For want of a nail the shoe was lost.',
             'And all for the want of a nail.'
         ];
-        $this->assertEquals($expected, $this->proverb->recite($pieces));
+        $this->assertEquals($expected, $proverb->recite($pieces));
     }
 
     /** @uuid c95ef757-5e94-4f0d-a6cb-d2083f5e5a83 */
     #[TestDox('Three pieces')]
     public function testThreePieces(): void
     {
+        $proverb = new Proverb();
         $pieces   = ['nail', 'shoe', 'horse'];
         $expected = [
             'For want of a nail the shoe was lost.',
             'For want of a shoe the horse was lost.',
             'And all for the want of a nail.'
         ];
-        $this->assertEquals($expected, $this->proverb->recite($pieces));
+        $this->assertEquals($expected, $proverb->recite($pieces));
     }
 
     /** @uuid 433fb91c-35a2-4d41-aeab-4de1e82b2126 */
     #[TestDox('Full proverb')]
     public function testFullProverb(): void
     {
+        $proverb = new Proverb();
         $pieces   = ['nail', 'shoe', 'horse', 'rider', 'message', 'battle', 'kingdom'];
         $expected = [
             'For want of a nail the shoe was lost.',
@@ -76,13 +74,14 @@ class ProverbTest extends TestCase
             'For want of a battle the kingdom was lost.',
             'And all for the want of a nail.'
         ];
-        $this->assertEquals($expected, $this->proverb->recite($pieces));
+        $this->assertEquals($expected, $proverb->recite($pieces));
     }
 
     /** @uuid c1eefa5a-e8d9-41c7-91d4-99fab6d6b9f7 */
     #[TestDox('Four pieces modernized')]
     public function testFourPiecesModernized(): void
     {
+        $proverb = new Proverb();
         $pieces   = ['pin', 'gun', 'soldier', 'battle'];
         $expected = [
             'For want of a pin the gun was lost.',
@@ -90,6 +89,6 @@ class ProverbTest extends TestCase
             'For want of a soldier the battle was lost.',
             'And all for the want of a pin.'
         ];
-        $this->assertEquals($expected, $this->proverb->recite($pieces));
+        $this->assertEquals($expected, $proverb->recite($pieces));
     }
 }
