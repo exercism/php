f53156d0fba5e2a7b636fbab8307b2e5ab48ee92
resu-xuniL <149326570+resu-xuniL@users.noreply.github.com>
2026-07-27 11:03:41 +0200

    Sync variable-length-quantity exercise (#1037)

--- exercises/practice/variable-length-quantity/.meta/example.php
+++ exercises/practice/variable-length-quantity/.meta/example.php
@@ -1,42 +1,12 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
-/**
- * @param array $integers
- * @return array
- */
-function vlq_encode(array $integers)
+function vlq_encode(array $integers): array
 {
     $result = [];
 
     foreach ($integers as $integer) {
-        if ($integer > 0xfffffff) {
-            throw new InvalidArgumentException('The value is greater than the maximum allowed.');
-        }
-
         $bytes = [];
         do {
             $byte = 0x7f & $integer;
@@ -49,11 +19,7 @@ function vlq_encode(array $integers)
     return $result;
 }
 
-/**
- * @param array $bytes
- * @return array
- */
-function vlq_decode(array $bytes)
+function vlq_decode(array $bytes): array
 {
     $result = [];
     $integer = 0;
@@ -73,7 +39,7 @@ function vlq_decode(array $bytes)
     }
 
     if (($byte & 0x80) !== 0) {
-        throw new InvalidArgumentException('Incomplete byte sequence.');
+        throw new InvalidArgumentException('Error: incomplete sequence.');
     }
 
     return $result;
