90398e369e1353bc46fe6aab236afd22aa151e93
resu-xuniL <149326570+resu-xuniL@users.noreply.github.com>
2026-08-30 07:53:19 +0200

    Add split-second-stopwatch exercise (#1048)

---
+++ exercises/practice/split-second-stopwatch/.meta/example.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+class SplitSecondStopwatch
+{
+    public function __construct(
+        public string $state = "ready",
+        public int $total = 0,
+        public int $currentLap = 0,
+        public array $previousLaps = []
+    ) {
+    }
+
+    public function advanceTime(string $duration): void
+    {
+        if ($this->state === 'running') {
+            $seconds = $this->formatSeconds($duration);
+            $this->currentLap += $seconds;
+            $this->total += $seconds;
+        }
+    }
+
+    private function formatTime(int $seconds): string
+    {
+        $hours = $seconds / 3600;
+        $minutes = ($seconds % 3600) / 60;
+        $seconds = $seconds % 60;
+
+        return sprintf("%02d:%02d:%02d", $hours, $minutes, $seconds);
+    }
+
+    private function formatSeconds(string $duration): int
+    {
+        [$hours, $minutes, $seconds] = explode(':', $duration);
+
+        return $hours * 3600 + $minutes * 60 + $seconds;
+    }
+
+    public function getCurrentLap(): string
+    {
+        return $this->formatTime($this->currentLap);
+    }
+
+    public function getTotal(): string
+    {
+        return $this->formatTime($this->total);
+    }
+
+    public function start(): void
+    {
+        if ($this->state === "running") {
+            throw new Exception("Error: cannot start an already running stopwatch");
+        }
+
+        $this->state = "running";
+    }
+
+    public function stop(): void
+    {
+        if ($this->state !== "running") {
+            throw new Exception("Error: cannot stop a stopwatch that is not running");
+        }
+
+        $this->state = "stopped";
+    }
+
+    public function lap(): void
+    {
+        if ($this->state !== "running") {
+            throw new Exception("Error: cannot lap a stopwatch that is not running");
+        }
+
+        $this->previousLaps[] = $this->getCurrentLap();
+        $this->currentLap = 0;
+    }
+
+    public function reset(): void
+    {
+        if ($this->state !== "stopped") {
+            throw new Exception("Error: cannot reset a stopwatch that is not stopped");
+        }
+
+        $this->state = "ready";
+        $this->currentLap = 0;
+        $this->previousLaps = [];
+    }
+}
